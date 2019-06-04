02ab5d53be9aad210d9a536807a4182f09ac57a1
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-06-04 17:05:07 +0100

    WIP code almost complete for generating, publishing and checking publications on internal

--- src/test/java/org/dvsa/testing/framework/stepdefs/PublicationsRelatedSteps.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/PublicationsRelatedSteps.java
@@ -1,6 +1,7 @@
 package org.dvsa.testing.framework.stepdefs;
 
 import activesupport.driver.Browser;
+import activesupport.number.Int;
 import cucumber.api.java.eo.Se;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.pages.BasePage;
@@ -10,34 +11,63 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PublicationsRelatedSteps extends BasePage implements En {
     public PublicationsRelatedSteps() {
         And("^i navigate to the admin publications page$", () -> {
             click("//*[contains(text(),'Admin')]",SelectorType.XPATH);
             click("//*[@id='menu-admin-dashboard/admin-publication']",SelectorType.XPATH);
         });
-        And("^i generate and publish all \"([^\"]*)\" publications$", (String cap) -> {
-            List<WebElement> radioButtons = Browser.getDriver().findElements(By.xpath("//*[@type='radio']"));
-            List<WebElement> publicationNumbers;
-            for (int i=0; i< Integer.parseInt(cap); i++) {
-]               waitAndClick("//*[text()='50']", SelectorType.XPATH);
-                waitForElementToBeClickable(String.valueOf(radioButtons.get(i)),SelectorType.XPATH);
-
-                click(String.valueOf(radioButtons.get(i)),SelectorType.XPATH);
-                waitAndClick("//*[@id='generate']",SelectorType.XPATH);
-
+        And("^i generate and publish all \"([^\"]*)\" publications$", (Integer noOfDifferentLicences) -> {
+            String currentPubNo;
+            String linkedPubNo;
+            for (int i=0; i< noOfDifferentLicences; i++) {
                 waitAndClick("//*[text()='50']", SelectorType.XPATH);
-                waitForElementToBeClickable(String.valueOf(radioButtons.get(i)),SelectorType.XPATH);
-
-                radioButtons = Browser.getDriver().findElements(By.xpath("//*[@type='radio']"));
-                publicationNumbers = Browser.getDriver().findElements(By.xpath("//table/tbody/tr/td[2]"));
-
-                assertTrue(isElementEnabled(String.valueOf(publicationNumbers.get(i+1)),SelectorType.XPATH));
-                click(String.valueOf(radioButtons.get(i+1)),SelectorType.XPATH);
 
 
-                waitAndClick(String.valueOf(radioButtons.get(i)),SelectorType.XPATH);
+                List<WebElement> radioButtons = Browser.getDriver().findElements(By.xpath("//*[@type='radio']"));
+                List<WebElement> publicationNumbers = Browser.getDriver().findElements(By.xpath("//table/tbody/tr[*]/td[2]"));
+                currentPubNo = publicationNumbers.get(i).getText();
 
+                if (Browser.getDriver().findElements(By.linkText(currentPubNo)).size()==0) {
+                    radioButtons.get(i).click();
+                    waitAndClick("//*[@id='generate']", SelectorType.XPATH);
+                    waitForTextToBePresent("Publication was generated, a new publication was also created");
+                    waitAndClick("//*[text()='50']", SelectorType.XPATH);
+
+                    radioButtons = Browser.getDriver().findElements(By.xpath("//*[@type='radio']"));
+                    publicationNumbers = Browser.getDriver().findElements(By.xpath("//table/tbody/tr/td[2]"));
+
+                    assertTrue(publicationNumbers.get(i + 1).isEnabled());
+                    linkedPubNo = publicationNumbers.get(i + 1).getText();
+                    radioButtons.get(i + 1).click();
+
+                    waitAndClick("//*[@id='publish']",SelectorType.XPATH);
+                    waitForTextToBePresent("Update successful");
+                    waitAndClick("//*[@id='menu-admin-dashboard/admin-publication/published']", SelectorType.XPATH);
+                    publicationNumbers = Browser.getDriver().findElements(By.xpath("//table/tbody/tr/td[2]"));
+
+                    List<String> textList;
+                    for (WebElement elements:publicationNumbers) {
+                        textList = elements.getText();
+                    }
+
+
+                    assertTrue(textList.contains(linkedPubNo));
+                    for (WebElement x : publicationNumbers) {
+                        if (publicationNumbers.toString().contains(linkedPubNo)) {
+                            assertTrue(isElementEnabled(String.valueOf(x), SelectorType.XPATH));
+                        }
+                    }
+
+                    click("//*[@id='menu-admin-dashboard/admin-publication/pending']", SelectorType.XPATH);
+                    waitForTextToBePresent("Publications");
+                }
+                else {
+                    i++;
+                    noOfDifferentLicences++;
+                }
             }
         });
     }
