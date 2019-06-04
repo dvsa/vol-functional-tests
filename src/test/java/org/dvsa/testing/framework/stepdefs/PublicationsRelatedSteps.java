6efb16e87a32ca1724e5a352dea14a4e15ad1039
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-06-04 10:28:38 +0100

    Commit to retrieve code ffrom another branch

--- src/test/java/org/dvsa/testing/framework/stepdefs/PublicationsRelatedSteps.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/PublicationsRelatedSteps.java
@@ -1,8 +1,14 @@
 package org.dvsa.testing.framework.stepdefs;
 
+import activesupport.driver.Browser;
+import cucumber.api.java.eo.Se;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.pages.BasePage;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class PublicationsRelatedSteps extends BasePage implements En {
     public PublicationsRelatedSteps() {
@@ -11,8 +17,14 @@ public class PublicationsRelatedSteps extends BasePage implements En {
             click("//*[@id='menu-admin-dashboard/admin-publication']",SelectorType.XPATH);
         });
         And("^i generate and publish all \"([^\"]*)\" publications$", (String cap) -> {
-            for (int i=1; i<= Integer.parseInt(cap); i++) {
-
+            waitAndClick("//*[text()='50']", SelectorType.XPATH);
+            List<WebElement> radioButtons = Browser.getDriver().findElements(By.xpath("//*[@type='radio']"));
+            Object[] radioButtonsArray = radioButtons.toArray();
+            for (int i=0; i< Integer.parseInt(cap); i++) {
+                waitAndClick("//*[text()='50']", SelectorType.XPATH);
+                click(String.valueOf(radioButtons.get(i)),SelectorType.XPATH);
+                waitAndClick("//*[@id='generate']",SelectorType.XPATH);
+                
             }
         });
     }
