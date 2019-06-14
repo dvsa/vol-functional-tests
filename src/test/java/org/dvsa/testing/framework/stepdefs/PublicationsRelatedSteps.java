8d779af6782483d133cae47273a6621dda22bb5c
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-06-14 09:50:51 +0100

    Fixed month function and test now works up until it gets onto publication page but can't find publications text

--- src/test/java/org/dvsa/testing/framework/stepdefs/PublicationsRelatedSteps.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/PublicationsRelatedSteps.java
@@ -25,7 +25,7 @@ public class PublicationsRelatedSteps extends BasePage implements En {
             String currentPubNo;
             String linkedPubNo;
             String publishedDate;
-            for (int i = 0; i < noOfDifferentLicences; i++) {
+            for (int i = 0; i < noOfDifferentLicences; i++) { //NEED TO CHANGE POSITION OF FOR OR IF. ELSE STATEMENT ADDS TWO TO i
                 // Pending Page
                 waitAndClick("//*[text()='50']", SelectorType.XPATH);
 
@@ -108,31 +108,43 @@ public class PublicationsRelatedSteps extends BasePage implements En {
         switch (monthNumber) {
         case "01":
             monthName = "January";
+            break;
         case "02":
             monthName = "February";
+            break;
         case "03":
             monthName = "March";
+            break;
         case "04":
             monthName = "April";
+            break;
         case "05":
             monthName = "May";
+            break;
         case "06":
             monthName = "June";
+            break;
         case "07":
             monthName = "July";
+            break;
         case "08":
             monthName = "August";
+            break;
         case "09":
             monthName = "September";
+            break;
         case "10":
             monthName = "October";
+            break;
         case "11":
             monthName = "November";
+            break;
         case "12":
             monthName = "December";
+            break;
         default:
             monthName = "Date Not Found";
-            return monthName;
         }
+        return monthName;
     }
 }
