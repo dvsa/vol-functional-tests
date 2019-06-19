d8ad268060a6b723a84dc36303e05dfa0a4fc449
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-06-19 11:13:05 +0100

    Better if statement using url to check if variation or new opposed to checking if text exists before waiting for it.

--- src/test/java/org/dvsa/testing/framework/stepdefs/TmVerifyDifferentOperator.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/TmVerifyDifferentOperator.java
@@ -118,9 +118,9 @@ public class TmVerifyDifferentOperator extends BasePage implements En {
             clickByLinkText("Sign out");
             world.UIJourneySteps.navigateToExternalUserLogin(world.createLicence.getLoginId(), world.createLicence.getEmailAddress());
             clickByLinkText(world.createLicence.getApplicationNumber());
-            if(Browser.getDriver().findElements(By.xpath("//*[contains(text(),'Apply for a new licence')]")).size()>0) {
+            if(Browser.getDriver().getCurrentUrl().contains("application")) {
                 waitForTextToBePresent("Apply for a new licence");
-            } else if (Browser.getDriver().findElements(By.xpath("//*[contains(text(),'Apply to change a licence')]")).size()>0) { // Is there a better check for if the licence is variation or new or not?
+            } else if (Browser.getDriver().getCurrentUrl().contains("variation")) {
                 waitForTextToBePresent("Apply to change a licence");
             }
             clickByLinkText("Transport");
