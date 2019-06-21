fa8009983778ac87dd983ef83b43d84a2da4aad0
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-06-21 15:30:57 +0100

    Fixed urlSearch function. Had URL.build set to External, not internal.

--- src/test/java/org/dvsa/testing/framework/Journeys/UIJourneySteps.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/UIJourneySteps.java
@@ -218,12 +218,12 @@ public class UIJourneySteps extends BasePage {
     }
 
     public void urlSearchAndViewApplication() throws IllegalBrowserException, MalformedURLException {
-        String myURL = URL.build(ApplicationType.EXTERNAL, env).toString();
+        String myURL = URL.build(ApplicationType.INTERNAL, env).toString();
         Browser.navigate().get(myURL.concat(String.format("application/%s",world.createLicence.getApplicationNumber())));
     }
 
     public void urlSearchAndViewLicence() throws IllegalBrowserException, MalformedURLException {
-        String myURL = URL.build(ApplicationType.EXTERNAL, env).toString();
+        String myURL = URL.build(ApplicationType.INTERNAL, env).toString();
         Browser.navigate().get(myURL.concat(String.format("licence/%s",world.createLicence.getLicenceId())));
     }
 
