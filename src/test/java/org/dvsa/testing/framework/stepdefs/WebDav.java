83ded6e0c913f8f56278727527d49c3ddc076836
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-09-12 11:11:59 +0100

    WIP: Added environment switch statement

--- src/test/java/org/dvsa/testing/framework/stepdefs/WebDav.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/WebDav.java
@@ -1,15 +1,29 @@
 package org.dvsa.testing.framework.stepdefs;
 
 import Injectors.World;
+import activesupport.system.Properties;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.pages.BasePage;
+import org.dvsa.testing.lib.url.utils.EnvironmentType;
 
 public class WebDav extends BasePage implements En {
+
+    private EnvironmentType env = EnvironmentType.getEnum(Properties.get("env", true));
+
     public WebDav(World world) {
         Then("^I check the document has been generated$", () -> {
+            switch (env.toString()) {
+                case "DA":
+                    break;
+                case "Demo":
+                    break;
+                    default:
+                        throw new Exception();
+            }
 //            GenericUtils.writeLineToFile();
         });
         And("^i write to a file the necessary information$", () -> {
+
         });
         And("^I check the change has been made to the document$", () -> {
 
