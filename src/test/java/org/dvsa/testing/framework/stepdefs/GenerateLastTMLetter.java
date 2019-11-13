804799676c3b93b538a754eaa42829d758689e29
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-11-13 16:55:36 +0000

    config initialisation for db tests taken out of activate support and properties before db function runs.

--- src/test/java/org/dvsa/testing/framework/stepdefs/GenerateLastTMLetter.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/GenerateLastTMLetter.java
@@ -1,13 +1,17 @@
 package org.dvsa.testing.framework.stepdefs;
 
 import Injectors.World;
+import activesupport.config.Configuration;
 import activesupport.database.DBUnit;
 import activesupport.jenkins.Jenkins;
 import activesupport.jenkins.JenkinsParameterKey;
 import activesupport.system.Properties;
+import com.typesafe.config.Config;
 import cucumber.api.java8.En;
+import io.restassured.internal.NoParameterValue;
 import org.dvsa.testing.lib.pages.BasePage;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
+import org.dvsa.testing.lib.url.utils.EnvironmentType;
 
 import java.sql.ResultSet;
 import java.util.HashMap;
@@ -19,10 +23,19 @@ public class GenerateLastTMLetter extends BasePage implements En {
 
     public GenerateLastTMLetter(World world) {
 
+        EnvironmentType env = EnvironmentType.getEnum(Properties.get("env", true));
+
+
         Given("^i have a valid \"([^\"]*)\" \"([^\"]*)\" licence$", (String operatorType, String licenceType) -> {
             world.UIJourneySteps.createLicence(world, operatorType, licenceType);
         });
         Then("^a flag should be set in the DB$", () -> {
+            Config config = new Configuration(env.toString()).getConfig();
+            if (config.getString("dbUsername").isEmpty() || config.getString("dbPassword").isEmpty()){
+                throw new Exception("No values for 'dbUsername' and 'dbPassword' from the config file.")
+            }
+            Properties.set("dbUsername",config.getString("dbUsername"));
+            Properties.set("dbPassword",config.getString("dbPassword"));
             ResultSet resultSet = DBUnit.checkResult(String.format("SELECT opt_out_tm_letter FROM OLCS_RDS_OLCSDB.licence\n" +
                     "WHERE lic_no='%s';", world.createLicence.getLicenceNumber()));
             if (resultSet.next()) {
