a83ac66c8cbda395d72af2ae4fd4a2f58b993d7c
whitehousea <ash4669@hotmail.co.uk>
2020-02-06 16:10:02 +0000

    fixed type error

--- src/test/java/org/dvsa/testing/framework/stepdefs/CreateApplications.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/CreateApplications.java
@@ -64,7 +64,8 @@ public class CreateApplications extends BasePage implements En {
         And("^i choose to pay my second application with my saved card details$", () -> {
             clickByLinkText("Home");
             Browser.navigate().findElements(By.xpath("//*[@class='table__wrapper'][last()]//td")).stream().skip(1).findAny().ifPresent(WebElement::click);
-            world.UIJourneySteps.navigateThroughApplication();click("//*[contains(text(),'Print')]", SelectorType.XPATH);
+            world.UIJourneySteps.navigateThroughApplication();
+            click("//*[contains(text(),'Print')]", SelectorType.XPATH);
             click("//*[@name='form-actions[submitAndPay]']", SelectorType.XPATH);
             selectValueFromDropDownByIndex("storedCards[card]", SelectorType.NAME, 1);
             waitAndClick("form-actions[pay]", SelectorType.NAME);
