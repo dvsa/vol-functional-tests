947385a1a1dfc5615d4565b6b76e33bb72eb477d
whitehousea <ash4669@hotmail.co.uk>
2020-05-04 16:50:13 +0100

    psv disc test fixed. Counts back from endNumber instead of counting forward from start number.

--- src/test/java/org/dvsa/testing/framework/stepdefs/LicenceDiscs.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/LicenceDiscs.java
@@ -10,14 +10,13 @@ public class LicenceDiscs extends BasePage implements En {
     public LicenceDiscs(World world) {
         And("^the licence discs should be present$", () -> {
 
-//          Retrieving starting print number and the number of discs going to be displayed on the licence.
-            int startNumber = Integer.valueOf(world.updateLicence.getStartNumber());
+//          Retrieving end print number and the number of discs going to be displayed on the licence.
+            int endNumber = Integer.valueOf(world.updateLicence.getEndNumber());
             int numberOfDiscs = world.createLicence.getNoOfVehiclesRequired();
 
-//          Looping over the discs numbers that should display on the licence, incrementing the disc number each time
-//          since they were created simultaneously and then asserting their presence.
-            for (int i = 0; i < numberOfDiscs; i++){
-                Assert.assertTrue(isTextPresent(String.valueOf(startNumber + i),5));
+//          Getting the end number and counting backwards as this will be the most recent set of licence disc that get printed.
+            for (int i = 1; i <= numberOfDiscs; i++){
+                Assert.assertTrue(isTextPresent(String.valueOf(endNumber - i),5));
             }
         });
     }
