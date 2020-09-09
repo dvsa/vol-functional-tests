5025a29de4680b5c12f8cfa9e7f8b11dd0163a32
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2020-09-09 16:00:42 +0100

    dvla manage users page

---
+++ src/test/java/org/dvsa/testing/framework/stepdefs/ManageVehicle.java
@@ -0,0 +1,48 @@
+package org.dvsa.testing.framework.stepdefs;
+
+import Injectors.World;
+import activesupport.IllegalBrowserException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import enums.OperatorType;
+import enums.UserRoles;
+
+import java.net.MalformedURLException;
+
+public class ManageVehicle {
+    World world;
+
+    public ManageVehicle(World world){
+         this.world = world;
+    }
+
+    @Given("I have a new {string} application")
+    public void iHaveANewApplication(String operatorType) {
+        world.createLicence.setOperatorType(OperatorType.getEnum(operatorType).getName());
+        world.APIJourneySteps.registerAndGetUserDetails(UserRoles.EXTERNAL.getUserRoles());
+        world.APIJourneySteps.createPartialApplication();
+    }
+
+    @When("I navigate to manage vehicle page")
+    public void iNavigateToManageVehiclePage() throws MalformedURLException, IllegalBrowserException {
+        world.selfServeNavigation.navigateToLogin( world.createLicence.getLoginId(), world.createLicence.getEmailAddress());
+        world.selfServeNavigation.navigateToPage("application", "review and declarations");
+    }
+
+    @Then("the add vehicle page should display licence number")
+    public void theAddVehiclePageShouldDisplayLicenceNumber() {
+
+    }
+
+    @And("choose to add a vehicle")
+    public void chooseToAddAVehicle() {
+        System.out.println("EXAMPLE");
+
+    }
+
+    @And("{string} heading")
+    public void heading(String arg0) {
+    }
+}
---
+++ src/test/resources/features/SelfServe/DVLA Data Integration/search-add-vehicle.feature
@@ -0,0 +1,14 @@
+Feature: Search and add a vehicle
+
+  Scenario Outline: Check page contents
+    Given I have a new "<Operator>" application
+    When I navigate to manage vehicle page
+    And choose to add a vehicle
+    Then the add vehicle page should display licence number
+    And "Add a vehicle" heading
+
+    Examples:
+      | Operator |
+      | goods    |
+      | public   |
+
