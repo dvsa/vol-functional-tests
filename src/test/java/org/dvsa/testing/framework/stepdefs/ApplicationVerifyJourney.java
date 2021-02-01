f64419d5c7fe4747b5735765cf71c6c9ebe0a72a
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-02-01 16:58:43 +0000

    undo navigate step

--- src/test/java/org/dvsa/testing/framework/stepdefs/ApplicationVerifyJourney.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/ApplicationVerifyJourney.java
@@ -22,13 +22,12 @@ public class ApplicationVerifyJourney extends BasePage {
 
     @Given("i have an application in progress")
     public void iHaveAnApplicationInProgress() throws MalformedURLException, IllegalBrowserException {
-        Browser.navigate().get("https://www.bbc.co.uk");
-//        world.createApplication.setOperatorType(OperatorType.PUBLIC.name());
-//        world.APIJourneySteps.registerAndGetUserDetails(UserType.EXTERNAL.asString());
-//        world.APIJourneySteps.createApplication();
-//        world.selfServeNavigation.navigateToLogin(world.registerUser.getUserName(), world.registerUser.getEmailAddress());
-//        world.selfServeNavigation.navigateToPage("application", "Review and declarations");
-//        world.UIJourneySteps.signDeclaration();
+        world.createApplication.setOperatorType(OperatorType.PUBLIC.name());
+        world.APIJourneySteps.registerAndGetUserDetails(UserType.EXTERNAL.asString());
+        world.APIJourneySteps.createApplication();
+        world.selfServeNavigation.navigateToLogin(world.registerUser.getUserName(), world.registerUser.getEmailAddress());
+        world.selfServeNavigation.navigateToPage("application", "Review and declarations");
+        world.UIJourneySteps.signDeclaration();
     }
 
     @When("i choose to sign with verify")
