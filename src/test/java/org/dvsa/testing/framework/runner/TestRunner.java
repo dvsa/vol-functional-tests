972e9bdedb5f0c20fd27960567cda6c694184594
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-02-01 13:44:50 +0000

    testNG class update

--- src/test/java/org/dvsa/testing/framework/runner/TestRunner.java
+++ src/test/java/org/dvsa/testing/framework/runner/TestRunner.java
@@ -1,23 +1,37 @@
 //package org.dvsa.testing.framework.runner;
 //
+//import Injectors.World;
+//import activesupport.IllegalBrowserException;
 //import activesupport.driver.Browser;
 //import cucumber.api.testng.AbstractTestNGCucumberTests;
 //import org.testng.annotations.*;
 //
 //import cucumber.api.CucumberOptions;
 //
+//import java.net.MalformedURLException;
+//
 //@CucumberOptions(features = {"src/test/resources/features"},
 //        glue = {"org.dvsa.testing.framework.stepdefs"})
 //
 //public class TestRunner extends AbstractTestNGCucumberTests {
 //
-//    public TestRunner() {
-//        Browser.setGridURL("https://username:password@hub-cloud.browserstack.com/wd/hub");
+//    private World world;
+//
+//    String browserStackUser = world.configuration.config.getString("browserStackUser");
+//    String browserStackKey = world.configuration.config.getString("browserStackKey");
+//
+//    public TestRunner( World world) {
+//        this.world = world;
 //    }
 //
-//    @BeforeMethod(alwaysRun = true)
+//    @BeforeClass
+//    public void setBrowserStackCredentials(){
+//        Browser.setGridURL(String.format("https://%s:%s@hub-cloud.browserstack.com/wd/hub",browserStackUser,browserStackKey));
+//    }
+//
+//    @BeforeTest(alwaysRun = true)
 //    @Parameters({ "browser", "version", "platform" })
-//    public void setUpClass(@Optional("edge") String browser, @Optional("87.0") String version, @Optional String platform) {
+//    public void setUpClass(String browser, String version, String platform) {
 //        System.out.println("===BROWSER==" + browser);
 //        System.out.println("===VERSION==" + version);
 //        Browser.setBrowserVersion(version);
@@ -27,5 +41,11 @@
 //    @DataProvider(parallel = true)
 //    public Object[][] scenarios() {
 //        return super.scenarios();
+//    }
+//
+//    @AfterClass
+//    public void tearDown() throws MalformedURLException, IllegalBrowserException {
+//        System.out.println("finished");
+////        Browser.navigate().quit();
 //    }
 //}
--- src/test/java/org/dvsa/testing/framework/stepdefs/ApplicationVerifyJourney.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/ApplicationVerifyJourney.java
@@ -5,6 +5,8 @@ import activesupport.IllegalBrowserException;
 import activesupport.driver.Browser;
 import apiCalls.enums.OperatorType;
 import apiCalls.enums.UserType;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -20,14 +22,12 @@ public class ApplicationVerifyJourney extends BasePage {
 
     @Given("i have an application in progress")
     public void iHaveAnApplicationInProgress() throws MalformedURLException, IllegalBrowserException {
-        Browser.navigate().get("https://www.bbc.co.uk");
-//            world.createApplication.setOperatorType(OperatorType.PUBLIC.name());
-//            world.APIJourneySteps.registerAndGetUserDetails(UserType.EXTERNAL.asString());
-//            world.APIJourneySteps.createApplication();
-//            world.selfServeNavigation.navigateToLogin(world.registerUser.getUserName(), world.registerUser.getEmailAddress());
-//            world.selfServeNavigation.navigateToPage("application", "Review and declarations");
-//            world.UIJourneySteps.signDeclaration();
-        assertTrue(Browser.navigate().getTitle().contains("BBC"));
+        world.createApplication.setOperatorType(OperatorType.PUBLIC.name());
+        world.APIJourneySteps.registerAndGetUserDetails(UserType.EXTERNAL.asString());
+        world.APIJourneySteps.createApplication();
+        world.selfServeNavigation.navigateToLogin(world.registerUser.getUserName(), world.registerUser.getEmailAddress());
+        world.selfServeNavigation.navigateToPage("application", "Review and declarations");
+        world.UIJourneySteps.signDeclaration();
     }
 
     @When("i choose to sign with verify")
