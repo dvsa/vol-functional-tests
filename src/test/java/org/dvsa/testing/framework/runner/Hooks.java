e91a5cbb1840c791f89748d3da97890b2346e225
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-02-02 17:34:52 +0000

    delete folder if exists

--- src/test/java/org/dvsa/testing/framework/runner/Hooks.java
+++ src/test/java/org/dvsa/testing/framework/runner/Hooks.java
@@ -3,14 +3,8 @@ package org.dvsa.testing.framework.runner;
 import activesupport.IllegalBrowserException;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
-import org.junit.Rule;
-import org.junit.rules.TestRule;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.TakesScreenshot;
@@ -28,14 +22,6 @@ public class Hooks {
 
     private void createDirectory() throws IOException {
         FileUtils.forceMkdir(directory);
-    }
-
-    @Before
-    public void deleteRunnersFolder(){
-        File runners = new File("src/test/java/runners");
-        if(runners.exists()){
-            runners.delete();
-        }
     }
 
     @Attachment(value = "Screenshot on failure", type = "image/png")
