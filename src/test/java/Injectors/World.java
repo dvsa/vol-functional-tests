fd9da8f51d0d34e38da9d782a7df915cbb603f4a
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-02-03 15:46:23 +0000

    readded class

--- src/test/java/Injectors/World.java
+++ src/test/java/Injectors/World.java
@@ -4,7 +4,6 @@ import apiCalls.actions.*;
 import org.dvsa.testing.framework.Global.Configuration;
 import org.dvsa.testing.framework.Global.GlobalMethods;
 import org.dvsa.testing.framework.Journeys.*;
-//import org.dvsa.testing.framework.Utils.API_CreateAndGrantAPP.GrantLicenceAPI;
 import org.dvsa.testing.framework.Utils.Generic.GenericUtils;
 
 public class World {
@@ -18,7 +17,6 @@ public class World {
     public RegisterUser registerUser;
     public GetUserDetails userDetails;
     public GrantLicence grantApplication;
-//    public GrantLicenceAPI grantLicence;
     public GenericUtils genericUtils;
     public UpdateLicence updateLicence;
     public DirectorJourneySteps directorJourneySteps;
