857f001c2f37716772a0e64dd1f777fd7ea211af
Ashley Whitehouse <ashley.whitehouse@dvsa.gov.uk>
2021-06-18 16:46:54 +0100

    refactored DeclarationPage to remove asserts. Deleted unused methods. Added more methods to DeclarationPageJourneySteps and begun refactoring the Journey methods.

---
+++ src/test/java/org/dvsa/testing/framework/Journeys/permits/external/pages/DeclarationPageJourneySteps.java
@@ -0,0 +1,30 @@
+package org.dvsa.testing.framework.Journeys.permits.external.pages;
+
+import org.dvsa.testing.framework.Journeys.permits.external.BasePermitJourney;
+import org.dvsa.testing.lib.newPages.permits.pages.DeclarationPage;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeclarationPageJourneySteps extends BasePermitJourney {
+
+    public static void hasPageHeading() {
+        String heading = DeclarationPage.getPageHeading();
+        assertEquals("Declaration", heading);
+    }
+
+    public static void completeDeclaration() {
+        DeclarationPage.untilOnPage();
+        DeclarationPage.confirmDeclaration();
+        DeclarationPage.saveAndContinue();
+    }
+
+    public static void hasCheckboxText() {
+        String checkboxText = DeclarationPage.getCheckboxText();
+        assertEquals("I confirm that I understand the conditions and that the information I have provided is true and correct to the best of my knowledge.", checkboxText);
+    }
+
+    public static void hasErrorText() {
+        String checkboxText = DeclarationPage.getErrorText();
+        assertEquals("Confirm you have read and agreed to this declaration", checkboxText);
+    }
+}
