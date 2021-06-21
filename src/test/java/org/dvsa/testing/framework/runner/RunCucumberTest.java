2c4cbd97d207aa561f4c5ec36824a45129495864
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-06-21 16:19:58 +0100

    testNF

--- src/test/java/org/dvsa/testing/framework/runner/RunCucumberTest.java
+++ src/test/java/org/dvsa/testing/framework/runner/RunCucumberTest.java
@@ -1,9 +1,14 @@
 package org.dvsa.testing.framework.runner;
 
+import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-public class RunCucumberTest extends AbstractTestNGCucumberTests{
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        glue = {"org.dvsa.testing.framework.stepdefs.vol"}
+)
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
     @Override
     @DataProvider(parallel = true)
--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/ApplicationVerifyJourney.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/ApplicationVerifyJourney.java
@@ -1,15 +1,12 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
 import Injectors.World;
-import activesupport.IllegalBrowserException;
 import apiCalls.enums.OperatorType;
 import apiCalls.enums.UserType;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 import org.dvsa.testing.lib.pages.BasePage;
-
-import java.net.MalformedURLException;
 
 import static org.dvsa.testing.framework.Utils.Generic.GenericUtils.getCurrentDate;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,19 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ApplicationVerifyJourney extends BasePage {
     World world;
 
-    public ApplicationVerifyJourney(World world){
+    public ApplicationVerifyJourney(World world) {
         this.world = world;
-    }
-
-    @Given("i have an application in progress")
-    public void iHaveAnApplicationInProgress() {
-        world.createApplication.setOperatorType(OperatorType.GOODS.name());
-        world.APIJourneySteps.registerAndGetUserDetails(UserType.EXTERNAL.asString());
-        world.APIJourneySteps.createApplication();
-        world.selfServeNavigation.navigateToLogin(world.registerUser.getUserName(), world.registerUser.getEmailAddress());
-        world.selfServeNavigation.navigateToPage("application", "Type of licence");
-        world.selfServeNavigation.navigateThroughApplication();
-        world.UIJourneySteps.signDeclaration();
     }
 
     @When("i choose to sign with verify")
@@ -42,5 +28,16 @@ public class ApplicationVerifyJourney extends BasePage {
         waitForTitleToBePresent("Review and declarations");
         assertTrue(isTextPresent("Declaration signed through GOV.UK Verify", 30));
         assertTrue(isTextPresent(String.format("Signed by Veena Pavlov on %s", getCurrentDate("dd MMM yyyy")), 30));
+    }
+
+    @Given("i have an application in progress")
+    public void iHaveAnApplicationInProgress() {
+        world.createApplication.setOperatorType(OperatorType.GOODS.name());
+        world.APIJourneySteps.registerAndGetUserDetails(UserType.EXTERNAL.asString());
+        world.APIJourneySteps.createApplication();
+        world.selfServeNavigation.navigateToLogin(world.registerUser.getUserName(), world.registerUser.getEmailAddress());
+        world.selfServeNavigation.navigateToPage("application", "Type of licence");
+        world.selfServeNavigation.navigateThroughApplication();
+        world.UIJourneySteps.signDeclaration();
     }
 }
