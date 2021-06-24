bb3a41d1c0d8ab482ebad0b46d6ac280a1964130
Ashley Whitehouse <ashley.whitehouse@dvsa.gov.uk>
2021-06-24 15:24:49 +0100

    TurkeyThirdCountryPage refactored. Asserts moved to functional-tests

--- src/test/java/org/dvsa/testing/framework/stepdefs/permits/bilateral/TurkeyThirdCountrySteps.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/permits/bilateral/TurkeyThirdCountrySteps.java
@@ -8,6 +8,7 @@ import org.dvsa.testing.lib.newPages.permits.pages.TurkeyThirdCountryPage;
 import org.junit.Assert;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TurkeyThirdCountrySteps implements En {
     public TurkeyThirdCountrySteps(OperatorStore operatorStore, World world) {
@@ -20,17 +21,21 @@ public class TurkeyThirdCountrySteps implements En {
             assertEquals(TurkeyThirdCountryPage.getCountry(),operatorStore.getCountry());
 
             //checking the Page heading on the Turkey third country page is correct
-            TurkeyThirdCountryPage.hasPageHeading();
+            assertEquals("Are you transporting goods from Turkey to a third-country?", TurkeyThirdCountryPage.getPageHeading());
 
             //checking the  YES and NO radio buttons  with display of respective help texts on seletion
-            TurkeyThirdCountryPage.hasCorrectRadioButtonsChoices();
+            assertTrue(TurkeyThirdCountryPage.areCorrectRadioButtonsPresent());
         });
 
         When("^I save and continue on the Turkey third country page$", TurkeyThirdCountryPage::saveAndContinue);
-        Then("^I should get the validation error message to select one option$", TurkeyThirdCountryPage::hasErrorText);
+        Then("^I should get the validation error message to select one option$", () -> {
+            assertEquals("Please select one option", TurkeyThirdCountryPage.getErrorText());
+        });
         When("^I select Yes radio button on the Turkey third country page$", TurkeyThirdCountryPage::clickYesToRadioButton);
         When("^I select No radio button on the Turkey third country page$", TurkeyThirdCountryPage::clickNoToRadioButton);
-        When("^I get advisory text that I don't need Turkey permits$", TurkeyThirdCountryPage::noSelectionAdvisoryText);
+        When("^I get advisory text that I don't need Turkey permits$", () -> {
+            assertTrue(TurkeyThirdCountryPage.noSelectionAdvisoryText());
+        });
         When("^I am navigated to the cancel application page$", () -> {
             CancellationPage.untilOnPage();
             String heading = CancellationPage.getPageHeading();
