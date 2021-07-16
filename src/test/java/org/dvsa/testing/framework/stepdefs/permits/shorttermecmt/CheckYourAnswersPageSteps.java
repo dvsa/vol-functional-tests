c13125269b717f7e0e3d34563a2fe369a816a06a
Ashley Whitehouse <ashley.whitehouse@dvsa.gov.uk>
2021-07-16 17:04:45 +0100

    short term check your answers page feature deleted

--- src/test/java/org/dvsa/testing/framework/stepdefs/permits/shorttermecmt/CheckYourAnswersPageSteps.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/permits/shorttermecmt/CheckYourAnswersPageSteps.java
@@ -38,53 +38,8 @@ public class CheckYourAnswersPageSteps implements En {
     public static Map<String, PermitUsage> permitUsage = new HashMap();
 
     public CheckYourAnswersPageSteps(OperatorStore operatorStore, World world) {
-
-        Then("^I am on the Short term check your answers page$", () -> {
-            clickToPermitTypePage(world);
-            ShorttermECMTJourney.getInstance().permitType(org.dvsa.testing.framework.enums.PermitType.SHORT_TERM_ECMT, operatorStore);
-            YearSelectionPage.selectShortTermValidityPeriod();
-            ShorttermECMTJourney.getInstance().shortTermType(PeriodType.ShortTermECMTAPSGWithSectors,operatorStore).licencePage(operatorStore,world);
-            LicenceStore licence = operatorStore.getLatestLicence().orElseGet(LicenceStore::new);
-            operatorStore.withLicences(licence);
-            OverviewPageJourney.clickOverviewSection(OverviewSection.HowWillYouUseThePermits);
-            licence.getEcmt().setPermitUsage(PermitUsage.random());
-            PermitUsagePage.permitUsage(licence.getEcmt().getPermitusage());
-            CheckYourAnswersPageSteps.permitUsage.put("permit.usage", licence.getEcmt().getPermitusage());
-            BasePermitPage.saveAndContinue();
-            CabotagePage.confirmWontUndertakeCabotage();
-            BasePermitPage.saveAndContinue();
-            CertificatesRequiredPage.completePage();
-            CountriesWithLimitedPermitsPage.noCountriesWithLimitedPermits();
-            NumberOfPermitsPageJourney.completeECMTPage();
-            EmissionStandardsPageJourney.completePage();
-            AnnualTripsAbroadPage.quantity(10);
-            BasePermitPage.saveAndContinue();
-            ProportionOfInternationalJourneyPage.chooseDesiredProportion(JourneyProportion.LessThan60Percent);
-            SectorPage.selectSectionAndContinue(Sector.random());
-        });
-        Then("^the short term check your answers page has reference number$", BasePermitPage::getReferenceFromPage);
-        Then("^Short term application answers are displayed on the check your answers page$", () -> {
-            String licence = CheckYourAnswerPage.getAnswer(Licence);
-            assertThat(licence, StringContains.containsString(operatorStore.getCurrentLicence().get().getLicenceNumber()));
-            String cabotage = CheckYourAnswerPage.getAnswer(Cabotage);
-            Assert.assertEquals("I confirm that I will not undertake cabotage journeys using an ECMT permit.", cabotage);
-            String certificates = CheckYourAnswerPage.getAnswer(CertificatesRequired);
-            Assert.assertEquals("I understand that I must obtain and carry the appropriate Certificate of Compliance and Certificate of Roadworthiness for each vehicle and trailer I intend to use with this permit.", certificates);
-            String permitType = CheckYourAnswerPage.getAnswer(PermitType);
-            Assert.assertEquals("Short-term ECMT",permitType );
-            String actualPermitUsage = CheckYourAnswerPage.getAnswer(PermitsUsage);
-            String s = toShortTermPermitUsage(CheckYourAnswersPageSteps.permitUsage.get("permit.usage"));
-            Assert.assertEquals(s,actualPermitUsage);
-        });
-        Then("^I am on the short term permits overview page with check your answers section marked as complete$", () -> {
-            OverviewPage.untilOnPage();
-            OverviewPageJourney.checkStatus(OverviewSection.CheckYourAnswers, PermitStatus.COMPLETED);
-        });
         Then("^the declaration section gets enabled to be clicked and section status changes to NOT STARTED YET$", () -> {
             OverviewPageJourney.checkStatus(OverviewSection.Declaration, PermitStatus.NOT_STARTED_YET);
-        });
-        And("^I click Check your answers link on the overview page again$", () -> {
-            OverviewPageJourney.clickOverviewSection(OverviewSection.CheckYourAnswers);
         });
     }
 
