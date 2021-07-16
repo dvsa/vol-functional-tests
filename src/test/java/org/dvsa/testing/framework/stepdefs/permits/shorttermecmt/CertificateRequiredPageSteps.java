8a8a171d1d4c6848ea2e1fc509828ea53598d99e
Ashley Whitehouse <ashley.whitehouse@dvsa.gov.uk>
2021-07-16 17:02:50 +0100

    short term certificates required page feature deleted

--- src/test/java/org/dvsa/testing/framework/stepdefs/permits/shorttermecmt/CertificateRequiredPageSteps.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/permits/shorttermecmt/CertificateRequiredPageSteps.java
@@ -25,35 +25,11 @@ import static org.junit.Assert.assertTrue;
 public class CertificateRequiredPageSteps implements En {
 
     public CertificateRequiredPageSteps(OperatorStore operatorStore, World world) {
-
-        Then("^I am on the shortterm certificates required page$", () -> {
-            clickToPermitTypePage(world);
-            ShorttermECMTJourney.getInstance().permitType(PermitType.SHORT_TERM_ECMT, operatorStore);
-            YearSelectionPage.selectShortTermValidityPeriod();
-            ShorttermECMTJourney.getInstance().shortTermType(PeriodType.ShortTermECMTAPSGWithSectors,operatorStore);
-            ShorttermECMTJourney.getInstance().licencePage(operatorStore,world);
-            OverviewPageJourney.clickOverviewSection(OverviewSection.HowWillYouUseThePermits);
-            PermitUsagePage.permitUsage(PermitUsage.random());
-            BasePermitPage.saveAndContinue();
-            CabotagePage.confirmWontUndertakeCabotage();
-            BasePermitPage.saveAndContinue();
-        });
-        Then("^the certificates required page has the relevant information$", () -> {
-            String heading = CertificatesRequiredPage.getPageHeading();
-            assertEquals("Mandatory certificates for vehicles and trailers you intend to use", heading);
-            CertificatesRequiredPage.getAdvisoryText(); // Create assert later.
-            String actualReferenceNumber = BasePermitPage.getReferenceFromPage();
-            Assert.assertTrue(actualReferenceNumber.contains(world.applicationDetails.getLicenceNumber()));
-        });
         Then("^I should get the certificates required page error message$", () -> {
             String errorText = CertificatesRequiredPage.getErrorText();
             assertEquals(errorText, "Tick to confirm you understand that each vehicle and trailer must have the matching certificates.");
         });
         Then("^I confirm the Certificates Required checkbox$", CertificatesRequiredPage::confirmCertificateRequired);
-        Then("^the user is navigated to the short term overview page with the status as completed$", () -> {
-            OverviewPage.untilOnPage();
-            OverviewPageJourney.checkStatus(OverviewSection.CertificatesRequired,PermitStatus.COMPLETED);
-        });
 
     }
 }
