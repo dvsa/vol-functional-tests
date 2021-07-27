f1d56254c1d235998a2d131255229d29185d06ae
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-07-27 11:33:35 +0100

    added testNG runner

--- src/test/java/org/dvsa/testing/framework/runner/Runner.java
+++ src/test/java/org/dvsa/testing/framework/runner/Runner.java
@@ -4,8 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-
-@CucumberOptions( features = {"src/test/resources/features"},
+@CucumberOptions(features = {"src/test/resources/features"},
         glue = {"org.dvsa.testing.framework.stepdefs"}
 )
 public class Runner extends AbstractTestNGCucumberTests {
