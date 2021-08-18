59716b73be76d45c8441ed7aa964bac17237754a
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-08-18 13:10:12 +0100

    EBSR path fix

--- src/test/java/org/dvsa/testing/framework/Utils/Generic/GenericUtils.java
+++ src/test/java/org/dvsa/testing/framework/Utils/Generic/GenericUtils.java
@@ -51,7 +51,7 @@ public class GenericUtils extends BasePage {
 
     public void modifyXML(String dateState, int months) {
         try {
-            String xmlFile = "./src/test/resources/ESBR/EBSR.xml";
+            String xmlFile = "./src/test/resources/EBSR/EBSR.xml";
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder xmlBuilder = documentBuilderFactory.newDocumentBuilder();
             Document xmlDoc = xmlBuilder.parse(xmlFile);
