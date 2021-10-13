59e93fd1a50a796282c7c1b9c8d34df684d48fa9
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-10-13 16:24:53 +0100

    add safety

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/VerifySwitchedOff.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/VerifySwitchedOff.java
@@ -132,7 +132,7 @@ public class VerifySwitchedOff extends BasePage {
         String myURL = URL.build(ApplicationType.EXTERNAL, world.configuration.env, "auth/login").toString();
 
         DriverUtils.get(myURL);
-        world.globalMethods.signIn("jacobfinney","");
+        world.globalMethods.signIn("jacobfinney", "");
         clickByXPath("//*[contains(text(),'Apply for a new licence')]");
         clickByXPath("//*[contains(text(),'Goods')]");
         clickByXPath("//*[contains(text(),'Standard National')]");
@@ -145,11 +145,11 @@ public class VerifySwitchedOff extends BasePage {
         waitForTitleToBePresent("Business details");
         waitAndClick(saveAndContinue, SelectorType.XPATH);
         waitForTitleToBePresent("Addresses");
-        waitAndEnterText("correspondence_address[searchPostcode][postcode]",SelectorType.NAME,"NG1 6LP");
+        waitAndEnterText("correspondence_address[searchPostcode][postcode]", SelectorType.NAME, "NG1 6LP");
         clickByName("correspondence_address[searchPostcode][search]");
-        waitAndSelectByIndex("Select an address","//*[@id='selectAddress1']",SelectorType.XPATH, 1);
-        waitAndEnterText("contact[phone_primary]",SelectorType.NAME,"07123456780");
-        waitAndEnterText("contact[email]",SelectorType.NAME,faker.bothify("????????##@volTest.org"));
+        waitAndSelectByIndex("Select an address", "//*[@id='selectAddress1']", SelectorType.XPATH, 1);
+        waitAndEnterText("contact[phone_primary]", SelectorType.NAME, "07123456780");
+        waitAndEnterText("contact[email]", SelectorType.NAME, faker.bothify("????????##@volTest.org"));
 
         waitAndClick(saveAndContinue, SelectorType.XPATH);
         waitForTitleToBePresent("Responsible people");
@@ -157,24 +157,70 @@ public class VerifySwitchedOff extends BasePage {
         waitAndClick(saveAndContinue, SelectorType.XPATH);
         waitForTitleToBePresent("Operating centres and authorisation");
         clickByXPath("//*[contains(text(),'Add operating centre')]");
-        waitAndEnterText("address[searchPostcode][postcode]",SelectorType.NAME,"B44 9UL");
+        waitAndEnterText("address[searchPostcode][postcode]", SelectorType.NAME, "B44 9UL");
         clickByName("address[searchPostcode][search]");
-        waitAndSelectByIndex("Select an address","//*[@id='selectAddress1']",SelectorType.XPATH, 1);
-        waitAndEnterText("data[noOfVehiclesRequired]",SelectorType.NAME,"6");
-        waitAndEnterText("data[noOfTrailersRequired]",SelectorType.NAME,"6");
+        waitAndSelectByIndex("Select an address", "//*[@id='selectAddress1']", SelectorType.XPATH, 1);
+        waitAndEnterText("data[noOfVehiclesRequired]", SelectorType.NAME, "6");
+        waitAndEnterText("data[noOfTrailersRequired]", SelectorType.NAME, "6");
         clickById("permission");
-        waitAndClick("//*[contains(text(),'Upload documents now')]",SelectorType.XPATH);
+        waitAndClick("//*[contains(text(),'Upload documents now')]", SelectorType.XPATH);
 
         uploadFile("//*[@id='advertisements[adPlacedContent][file][file]']", workingDir + financialEvidenceFile, "document.getElementById('advertisements[adPlacedContent][file][file]').style.left = 0", SelectorType.XPATH);
 
-        waitAndEnterText("adPlacedIn",SelectorType.ID, "VOL Tribune");
-        waitAndEnterText("adPlacedDate_day",SelectorType.ID, String.valueOf(LocalDate.now().getDayOfMonth()));
-        waitAndEnterText("adPlacedDate_month",SelectorType.ID, String.valueOf(LocalDate.now().getMonthValue()));
-        waitAndEnterText("adPlacedDate_year",SelectorType.ID, String.valueOf(LocalDate.now().getYear()));
+        waitAndEnterText("adPlacedIn", SelectorType.ID, "VOL Tribune");
+        waitAndEnterText("adPlacedDate_day", SelectorType.ID, String.valueOf(LocalDate.now().getDayOfMonth()));
+        waitAndEnterText("adPlacedDate_month", SelectorType.ID, String.valueOf(LocalDate.now().getMonthValue()));
+        waitAndEnterText("adPlacedDate_year", SelectorType.ID, String.valueOf(LocalDate.now().getYear()));
         waitAndClick("form-actions[submit]", SelectorType.NAME);
+        waitAndEnterText("data[totAuthVehicles]", SelectorType.NAME, "6");
+        waitAndEnterText("data[totAuthTrailers]", SelectorType.NAME, "6");
         waitAndClick(saveAndContinue, SelectorType.XPATH);
-        waitAndEnterText("data[totAuthVehicles]",SelectorType.NAME,"6");
-        waitAndEnterText("data[totAuthTrailers]",SelectorType.NAME,"6");
+
         waitForTitleToBePresent("Financial evidence");
+        waitAndClick("//*[contains(text(),'Send documents')]", SelectorType.XPATH);
+        waitAndClick(saveAndContinue, SelectorType.XPATH);
+
+        waitForTitleToBePresent("Transport Managers");
+        waitAndClick("add", SelectorType.ID);
+        selectValueFromDropDownByIndex( "data[registeredUser]", SelectorType.NAME, 1);
+        waitAndClick("form-actions[continue]", SelectorType.ID);
+
+        waitForTitleToBePresent("Transport Manager details");
+        waitAndEnterText("data[birthDate][day]", SelectorType.NAME, String.valueOf(LocalDate.now().getDayOfMonth()));
+        waitAndEnterText("data[birthDate][month]", SelectorType.NAME, String.valueOf(LocalDate.now().getMonthValue()));
+        waitAndEnterText("data[birthDate][year]", SelectorType.NAME, String.valueOf(LocalDate.now().minusYears(40).getYear()));
+        waitAndClick("form-actions[send]", SelectorType.NAME);
+        waitForTitleToBePresent("Transport Managers");
+        waitAndClick(saveAndContinue, SelectorType.XPATH);
+
+        waitForTitleToBePresent("Vehicle details");
+        clickById("add");
+
+        waitForTitleToBePresent("Add vehicle");
+        waitAndEnterText("vrm",SelectorType.ID,"DWK412T");
+        waitAndEnterText("plated_weight",SelectorType.ID,"5000");
+        waitAndClick("form-actions[submit]", SelectorType.ID);
+        waitForTitleToBePresent("Vehicle details");
+        waitAndClick(saveAndContinue, SelectorType.XPATH);
+
+        waitForTitleToBePresent("Safety and compliance");
+        waitAndEnterText("licence[safetyInsVehicles]",SelectorType.NAME,"10");
+        waitAndEnterText("licence[safetyInsTrailers]",SelectorType.NAME,"10");
+        waitAndClick("//*[contains(text(),'Yes')]",SelectorType.XPATH);
+        waitAndClick("//*[contains(text(),'In-house')]",SelectorType.XPATH);
+        waitAndClick("table[action]",SelectorType.NAME);
+
+        waitForTitleToBePresent("Add safety inspector");
+        waitAndClick("//*[contains(text(),'An owner')]",SelectorType.XPATH);
+        waitAndClick("//*[contains(text(),'An external')]",SelectorType.XPATH);
+        waitAndEnterText("contactDetails[fao]",SelectorType.NAME, faker.generateFirstName() + " " + faker.generateLastName());
+        waitAndEnterText("address[searchPostcode][postcode]",SelectorType.NAME,"NG1 6LP");
+        waitAndClick("address[searchPostcode][search]",SelectorType.NAME);
+        waitAndSelectByIndex("Add safety inspector", "selectAddress1",SelectorType.ID,1);
+        waitAndClick("form-actions[submit]",SelectorType.NAME);
+
+        waitForTitleToBePresent("Safety and compliance");
+        waitAndClick("//*[@id=\"application[safetyConfirmation]\"]",SelectorType.XPATH);
+        waitAndClick(saveAndContinue, SelectorType.XPATH);
     }
 }
