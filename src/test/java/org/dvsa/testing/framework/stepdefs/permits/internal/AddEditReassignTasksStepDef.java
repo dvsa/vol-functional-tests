394f7cd9278a560174f457c44644e45cdfcc4273
Neil Culshaw <neilculshaw@c02f90cdmd6m.local>
2021-11-11 12:52:05 +0000

    Fixed new assertions

--- src/test/java/org/dvsa/testing/framework/stepdefs/permits/internal/AddEditReassignTasksStepDef.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/permits/internal/AddEditReassignTasksStepDef.java
@@ -35,12 +35,21 @@ public class AddEditReassignTasksStepDef extends BasePage implements En {
     @Then("the User has re-assigned a task")
     public void theUserHasReAssignedATask() {
         Assert.assertTrue(isTextPresent("System Team (COPESTAKE, ANDREW DAVID)"));
+        Assert.assertTrue(isTextPresent("Forms Digital"));
+        Assert.assertTrue(isTextPresent("GV79 Application"));
+        String actualDate = getText("//tbody/tr[1]/td[4]", SelectorType.XPATH);
+        String expectedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Assert.assertEquals(expectedDate, actualDate);
     }
 
     @Then("the User has edited a task")
     public void theUserHasEditedATask() {
         Assert.assertTrue(isTextPresent("System Team (COPESTAKE, ANDREW DAVID)"));
         Assert.assertTrue(isTextPresent(world.adminJourney.getDescription()));
+        Assert.assertTrue(isTextPresent("Forms Digital"));
+        String actualDate = getText("//tbody/tr[1]/td[4]", SelectorType.XPATH);
+        String expectedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Assert.assertEquals(expectedDate, actualDate);
     }
 
     @Then("the User has added a task")
@@ -48,7 +57,7 @@ public class AddEditReassignTasksStepDef extends BasePage implements En {
           Assert.assertTrue(isTextPresent(world.adminJourney.getDescription()));
           String actualDate = getText("//tbody/tr[2]/td[4]", SelectorType.XPATH);
           String expectedDate = LocalDate.now().plusYears(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-         Assert.assertEquals(expectedDate, actualDate);
+          Assert.assertEquals(expectedDate, actualDate);
     }
 }
 
