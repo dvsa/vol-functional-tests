6bd699d6da13d6919467e26d20f36371e8e96b0e
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-11-30 17:28:45 +0000

    added navigation step

--- src/test/java/org/dvsa/testing/framework/Utils/Generic/GenericUtils.java
+++ src/test/java/org/dvsa/testing/framework/Utils/Generic/GenericUtils.java
@@ -134,8 +134,8 @@ public class GenericUtils extends BasePage {
         /*
         / Uses Open source util zt-zip https://github.com/zeroturnaround/zt-zip
          */
-        ZipUtil.pack(new File("./src/test/resources/EBSR"), new File(String.format("/src/test/resources/%s",fileName)));
-        return String.format("./src/test/resources/%s",fileName);
+        ZipUtil.pack(new File("./src/test/resources/EBSR"), new File(String.format("./src/test/resources/%s",fileName)));
+        return String.format("/src/test/resources/%s",fileName);
     }
 
     public void executeJenkinsBatchJob(String command) throws Exception {
