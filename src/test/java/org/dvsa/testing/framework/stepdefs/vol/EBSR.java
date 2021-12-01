776203b671de7456c30db31892f5ddc383da6ec0
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-12-01 15:38:09 +0000

    scratch volume

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/EBSR.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/EBSR.java
@@ -4,6 +4,8 @@ import Injectors.World;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 
+import java.io.IOException;
+
 public class EBSR {
     World world;
 
@@ -16,7 +18,7 @@ public class EBSR {
     }
 
     @When("I upload an ebsr file with {string} days notice")
-    public void iUploadAnEbsrFileWithDaysNotice(String days) {
+    public void iUploadAnEbsrFileWithDaysNotice(String days) throws IOException {
         // for the date state the options are ['current','past','future'] and depending on your choice the months you want to add/remove
         world.busRegistrationJourney.uploadAndSubmitEBSR("futureDay", Integer.parseInt(days));
     }
