94eba216ec8211af0bf1ae29c2e665438c94db28
Stefan Ranoszek <stefanranoszek@icloud.com>
2021-12-14 10:41:10 +0000

    updated pom, removed unused import statement

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/UpdateInternalUserAccount.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/UpdateInternalUserAccount.java
@@ -4,7 +4,6 @@ import Injectors.World;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import javafx.concurrent.Worker;
 import org.dvsa.testing.framework.pageObjects.BasePage;
 import org.dvsa.testing.framework.pageObjects.enums.AdminOption;
 import org.junit.Assert;
