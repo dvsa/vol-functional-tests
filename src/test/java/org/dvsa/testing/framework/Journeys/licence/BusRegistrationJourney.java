bb9c1c91158ac8873194b762490369cfe721f23a
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-12-23 16:32:12 +0000

    stream

--- src/test/java/org/dvsa/testing/framework/Journeys/licence/BusRegistrationJourney.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/licence/BusRegistrationJourney.java
@@ -204,7 +204,7 @@ public class BusRegistrationJourney extends BasePage {
                     FileUtils.copyURLToFile(
                             new URL("https://devapp-olcs-pri-olcs-autotest-s3.s3.eu-west-1.amazonaws.com/BusReg/"
                                     .concat(ebsrFileName)),
-                            new File("/tmp/testing"),
+                            new File(System.getProperty("user.dir").concat("/tmp/testing_downloads")),
                             2000,
                             2000);
                 }catch (Exception io){
