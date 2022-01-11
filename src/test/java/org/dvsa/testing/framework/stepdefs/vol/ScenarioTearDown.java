05b96bb22b33f76f11f4aaef4c01633947de5bd1
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-01-11 09:18:37 +0000

    single fork

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/ScenarioTearDown.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/ScenarioTearDown.java
@@ -1,6 +1,5 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
-import activesupport.driver.Browser;
 import activesupport.driver.BrowserStack;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
@@ -9,13 +8,11 @@ import org.dvsa.testing.framework.Report.Config.Environments;
 import org.dvsa.testing.framework.runner.Hooks;
 
 
-import java.io.IOException;
-
-
 public class ScenarioTearDown {
     @After
     public void afterClass(Scenario scenario) throws Exception {
         Hooks.attach(scenario);
+        BrowserStack.stopLocal();
     }
 
     @Before
