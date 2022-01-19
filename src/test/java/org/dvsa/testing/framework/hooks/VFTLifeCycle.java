38c8f41402a6cd5f20e1256a2a7aca77e719c0d3
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-01-19 12:34:49 +0000

    pom changes

--- src/test/java/org/dvsa/testing/framework/hooks/VFTLifeCycle.java
+++ src/test/java/org/dvsa/testing/framework/hooks/VFTLifeCycle.java
@@ -2,13 +2,13 @@ package org.dvsa.testing.framework.hooks;
 
 import activesupport.driver.Browser;
 import activesupport.driver.BrowserStack;
-import cucumber.api.Scenario;
 import cucumber.api.event.EventListener;
 import cucumber.api.event.EventPublisher;
 import cucumber.api.event.TestRunFinished;
 import cucumber.api.event.TestRunStarted;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 
 public class VFTLifeCycle implements EventListener {
     private static final Logger LOGGER = LogManager.getLogger(VFTLifeCycle.class);
@@ -19,13 +19,14 @@ public class VFTLifeCycle implements EventListener {
         });
         publisher.registerHandlerFor(TestRunFinished.class, event -> {
             LOGGER.info("Test is shutting down");
-            if(Browser.isBrowserOpen() && (!Browser.navigate().getTitle().equals(""))){
-            try {
-                Browser.closeBrowser();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+            System.out.println("THIS IS THE TITLE:" + Browser.navigate().getTitle());
+//            if(Browser.isBrowserOpen()){
+//            try {
+//                Browser.closeBrowser();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
         });
     }
 }
