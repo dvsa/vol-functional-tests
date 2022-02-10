9644d690b0e1d5c8b653d9b27b8610cb945aa44b
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-02-10 10:40:12 +0000

    publications

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/PublicHolidays.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/PublicHolidays.java
@@ -31,7 +31,7 @@ public class PublicHolidays extends BasePage {
     @Then("that holiday should be displayed")
     public void thatHolidayShouldBeDisplayed() {
         String actualDate = getValue("(//input[@type='submit'])[2]", SelectorType.XPATH);
-        String expectedDate = LocalDate.now().plusYears(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String expectedDate = LocalDate.now().plusDays(1).plusYears(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         waitForTitleToBePresent("Public holidays");
         Assert.assertEquals(expectedDate, actualDate);
     }
