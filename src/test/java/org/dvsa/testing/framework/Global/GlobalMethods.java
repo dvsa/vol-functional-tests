99d4df66665f1a58862d6be0993261ac97e606d4
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-03-02 12:52:55 +0000

    verify fix

--- src/test/java/org/dvsa/testing/framework/Global/GlobalMethods.java
+++ src/test/java/org/dvsa/testing/framework/Global/GlobalMethods.java
@@ -44,6 +44,13 @@ public class GlobalMethods extends BasePage {
     public void navigateToLoginWithoutCookies(String username, String emailAddress, ApplicationType applicationType) {
         String newPassword = world.configuration.config.getString("internalNewPassword");
         String myURL = URL.build(applicationType, world.configuration.env, "auth/login").toString();
+        if (Browser.isBrowserOpen()) {
+            navigate().manage().deleteAllCookies();
+            navigate().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+            if (isElementPresent("//*[contains(text(),'Accept')]", SelectorType.XPATH)) {
+                waitAndClick("//*[contains(text(),'Accept')]", SelectorType.XPATH);
+            }
+        }
         DriverUtils.get(myURL);
         enterCredentialsAndLogin(username, emailAddress, newPassword);
     }
@@ -52,26 +59,19 @@ public class GlobalMethods extends BasePage {
         // TODO: Setup way to store new passwords after they are set and once they are set default to them?
         // Also look at calls in SS and Internal Navigational steps cause there is a lot of replication.
         String password = world.configuration.getTempPassword(emailAddress);
-        navigate().manage().deleteAllCookies();
-        if (isElementPresent("//*[contains(text(),'Accept')]", SelectorType.XPATH)) {
-            waitAndClick("//*[contains(text(),'Accept')]", SelectorType.XPATH);
-        }
         try {
-            if (isTitlePresent("Sign in to your Vehicle Operator Licensing account", 1)) {
-                signIn(username, password);
-            }
+            signIn(username, password);
         } catch (Exception e) {
             //User is already registered
-            if (isTitlePresent("Sign in to your Vehicle Operator Licensing account", 1)) {
-                signIn(username, getLoginPassword());
-            }
+            signIn(username, getLoginPassword());
         } finally {
-            if (isTitlePresent("Change your password", 1)) {
+            if (isTitlePresent("Change your password",3)) {
                 waitAndEnterText(oldPasswordField, SelectorType.CSS, password);
                 waitAndEnterText(newPasswordField, SelectorType.CSS, newPassword);
                 waitAndEnterText(confirmPasswordField, SelectorType.CSS, newPassword);
                 click(nameAttribute("input", "submit"), SelectorType.CSS);
                 setLoginPassword(newPassword);
+                untilNotInDOM(submitButton, 5);
             }
         }
     }
