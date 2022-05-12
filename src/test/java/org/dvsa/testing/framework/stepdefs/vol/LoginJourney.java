8c07393898443f51714491dc916e7b7f77a5c478
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-05-12 12:21:03 +0100

    login journey

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/LoginJourney.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/LoginJourney.java
@@ -1,28 +1,52 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
+import Injectors.World;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
+import org.dvsa.testing.framework.pageObjects.BasePage;
 
-public class LoginJourney {
+import java.util.List;
+
+public class LoginJourney extends BasePage {
+    World world = new World();
+    String userId;
+
     @When("I attempt to login with a username with special characters")
-    public void iAttemptToLoginWithAUsernameWithSpecialCharacters() {
+    public void iAttemptToLoginWithAUsernameWithSpecialCharacters(DataTable userTable) {
+        List<String> specialCharacterUser = userTable.asList();
+        for (String users : specialCharacterUser) {
+            world.globalMethods.signIn(users, world.configuration.config.getString("internalNewPassword"));
+            this.userId = users;
+        }
     }
 
     @Then("I should be authenticated")
     public void iShouldBeAuthenticated() {
+        isTextPresent(this.userId);
     }
 
     @And("redirected to the dashboard")
     public void redirectedToTheDashboard() {
+        isTextPresent("logout");
     }
 
     @When("I attempt to login with a username with an email format")
-    public void iAttemptToLoginWithAUsernameWithAnEmailFormat() {
-
+    public void iAttemptToLoginWithAUsernameWithAnEmailFormat(DataTable userTable) {
+        List<String> usersWithEmailFormatUsername = userTable.asList();
+        for (String users : usersWithEmailFormatUsername) {
+            world.globalMethods.signIn(users, world.configuration.config.getString("internalNewPassword"));
+            this.userId = users;
+        }
     }
 
     @When("I attempt to login with users that share the same email address")
-    public void iAttemptToLoginWithUsersThatShareTheSameEmailAddress() {
+    public void iAttemptToLoginWithUsersThatShareTheSameEmailAddress(DataTable userTable) {
+        List<String> usersThatShareEmailAddresses = userTable.asList();
+        for(String users : usersThatShareEmailAddresses){
+            world.globalMethods.signIn(users, world.configuration.config.getString("internalNewPassword"));
+            this.userId = users;
+        }
     }
 }
--- src/test/resources/features/licences/SelfServe/User Account/login.feature
+++ src/test/resources/features/licences/SelfServe/User Account/login.feature
@@ -1,19 +1,53 @@
+@VOL-3129
 Feature: Login into self service
 
   Scenario: Dodgy characters in login id
     Given I am on Selfserve homepage
     When I attempt to login with a username with special characters
+      | user |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
     Then I should be authenticated
     And redirected to the dashboard
 
   Scenario: Email like login id’s
     Given I am on Selfserve homepage
     When I attempt to login with a username with an email format
+      | user |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
     Then I should be authenticated
     And redirected to the dashboard
 
   Scenario: Users with the same email address
     Given I am on Selfserve homepage
     When I attempt to login with users that share the same email address
+      | user |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
+      |      |
     Then I should be authenticated
     And redirected to the dashboard
