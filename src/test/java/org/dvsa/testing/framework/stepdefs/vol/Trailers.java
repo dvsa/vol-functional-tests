14fc77d03d671c7f22e9ee5943b6b69200a318b3
Andrew.Platts <andrew.platts@bjss.com>
2022-06-06 11:31:37 +0100

    added semi trailer check

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/Trailers.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/Trailers.java
@@ -1,21 +1,14 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
 import Injectors.World;
-import apiCalls.enums.VehicleType;
 import cucumber.api.java.en.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dvsa.testing.framework.Journeys.licence.UIJourney;
-import org.dvsa.testing.framework.Journeys.permits.AnnualBilateralJourney;
-import org.dvsa.testing.framework.enums.Duration;
 import org.dvsa.testing.framework.enums.SelfServeSection;
 import org.dvsa.testing.framework.pageObjects.BasePage;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
 import org.junit.Assert;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -35,29 +28,12 @@ public class Trailers extends BasePage {
         world.trailersJourney.addTrailerToLicence(trailerNumber);
         world.trailersJourney.isLongerSemiTrailer(isLongerSemiTrailer);
         waitAndClick(submitButton, SelectorType.XPATH);
-        //String Selector = "//input[@name='data[longerSemiTrailer][isLongerSemiTrailer]']";
-        //untilElementIsPresent(Selector, SelectorType.XPATH, Duration.MEDIUM, TimeUnit.SECONDS);
-        //clickByXPath(Selector + "[@value='Y']");
-        //scrollAndClick(Selector,SelectorType.XPATH);
     }
 
-    @When("the trailer {string} is successfully added to the trailer table")
-    public void trailerAddedCheck(String trailerNumber) {
+    @When("the trailer {string} and type {string} is successfully added to the trailer table")
+    public void trailerAddedCheck(String trailerNumber, String isLongerSemiTrailer) {
         assertTrue(isElementPresent("//tbody/tr/td/input[@value='" + trailerNumber + "']", SelectorType.XPATH));
-
-        //List<WebElement> trs = findElements("//tbody/tr/td", SelectorType.XPATH);
-        //for (int i = 0; i < trs.size(); i++) {
-        //    LOGGER.info("Each open tab ID : " + trs.get(i).getText());
-        //}
-        //List<WebElement> trs1 = findElements("//tbody/tr/td[@data-heading='Trailer number']", SelectorType.XPATH);
-        //String text = getText("//tbody/tr/td[@data-heading='Trailer number']",SelectorType.XPATH);
-        //String text1 = getText("//tbody/tr/td[1][@data-heading='Trailer number']",SelectorType.XPATH);
-        //String text2 = getText("//tbody/tr/td[1]/input",SelectorType.XPATH);
-        //String text3 = getText("//tbody/tr/td[0][@data-heading='Trailer number']",SelectorType.XPATH);
-        //String text4 = getText("/input[@type='submit']",SelectorType.XPATH);
-
-        //Assert.assertEquals(getElementValueByText("//tbody/tr/td[@data-heading='Country']",SelectorType.XPATH), AnnualBilateralJourney.getCountry());
-        //Assert.assertEquals(trailerNumber, getElementValueByText("//tbody/tr/td[@data-heading='Trailer number']", SelectorType.XPATH));
+        Assert.assertEquals(getElementValueByText("//tbody/tr/td[@data-heading='Longer semi-trailer']",SelectorType.XPATH), isLongerSemiTrailer);
         UIJourney.clickSaveAndReturn();
     }
 
--- src/test/resources/features/licences/SelfServe/Longer Semi Trailers/add-trailers-to-licence.feature
+++ src/test/resources/features/licences/SelfServe/Longer Semi Trailers/add-trailers-to-licence.feature
@@ -4,7 +4,7 @@ Feature: Adding trailers to a licence - Longer semi and non longer semi trailers
   Scenario Outline: Adding longer semi trailers to a licence
     Given I have a "goods" "<licenceType>" licence
     When I add a valid trailer number "<trailerNumber>" and longer semi trailer is set to "<semiTrailer>" on the licence
-    Then the trailer "<trailerNumber>" is successfully added to the trailer table
+    Then the trailer "<trailerNumber>" and type "<semiTrailer>" is successfully added to the trailer table
 
 
     Examples:
@@ -16,7 +16,7 @@ Feature: Adding trailers to a licence - Longer semi and non longer semi trailers
   Scenario Outline: Adding non longer semi trailers to a licence
     Given I have a "goods" "<licenceType>" licence
     And I add a valid trailer number "<trailerNumber>" and longer semi trailer is set to "<semiTrailer>" on the licence
-    Then the trailer "<trailerNumber>" is successfully added to the trailer table
+    Then the trailer "<trailerNumber>" and type "<semiTrailer>" is successfully added to the trailer table
 
 
     Examples:
