098c85e0b6108655fce8e7af9c591027b0363263
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-06-08 10:18:19 +0100

    fixed runner issue

--- src/test/java/org/dvsa/testing/framework/hooks/VFTLifeCycle.java
+++ src/test/java/org/dvsa/testing/framework/hooks/VFTLifeCycle.java
@@ -1,6 +1,5 @@
 package org.dvsa.testing.framework.hooks;
 
-import activesupport.driver.Browser;
 import cucumber.api.event.EventListener;
 import cucumber.api.event.EventPublisher;
 import cucumber.api.event.TestRunFinished;
