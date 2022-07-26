2093d585a4a1f48af4cf575270ddcc9afa6b4a40
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2022-07-26 14:35:39 +0100

    increase timeout

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/EBSRUpload.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/EBSRUpload.java
@@ -80,7 +80,7 @@ public class EBSRUpload extends BasePage implements En {
     @And("Documents are generated")
     public void documentsAreGenerated() {
         waitAndClick(String.format("//*[contains(text(),'%s')]", world.applicationDetails.getLicenceNumber()), SelectorType.XPATH);
-        long kickOutTime = System.currentTimeMillis() + 30000;
+        long kickOutTime = System.currentTimeMillis() + 200000;
         do {
             // Refresh page
             refreshPageWithJavascript();
