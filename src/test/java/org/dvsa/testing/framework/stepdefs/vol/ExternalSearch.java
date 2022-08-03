5ad2803a98121a71c5ebb5f2a06e93fb0b16dcd2
Andrew.Platts <andrew.platts@bjss.com>
2022-08-03 13:27:29 +0100

    increased kick out time

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/ExternalSearch.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/ExternalSearch.java
@@ -106,7 +106,7 @@ public class ExternalSearch extends BasePage implements En {
     @Then("search results page should display names containing our operator name")
     public void searchResultsPageShouldDisplayNamesContainingOurOperatorName() {
         String operatorName = String.format("%s %s", world.createApplication.getDirectorForeName(), world.createApplication.getDirectorFamilyName());
-        world.selfServeNavigation.clickSearchWhileCheckingTextPresent(operatorName, 300, "KickOut reached. Operator name external search failed.");
+        world.selfServeNavigation.clickSearchWhileCheckingTextPresent(operatorName, 420, "KickOut reached. Operator name external search failed.");
         WebElement tableRow = findElement(String.format("//tr[td[contains(text(),\"%s\")]]", operatorName), SelectorType.XPATH);
         assertTrue(tableRow.getText().contains(world.createApplication.getOrganisationName()));
         assertTrue(tableRow.getText().contains(world.applicationDetails.getLicenceNumber()));
