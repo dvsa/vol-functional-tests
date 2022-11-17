272143a93243832c0ae88d853ad15679584e7c17
Wonderson Chideya <wondersonchideya@gmail.com>
2022-11-17 16:22:08 +0000

    methods

--- src/test/java/org/dvsa/testing/framework/parallel/RunCucumberTests.java
+++ src/test/java/org/dvsa/testing/framework/parallel/RunCucumberTests.java
@@ -1,18 +1,15 @@
 package org.dvsa.testing.framework.parallel;
 
-
-
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("org/dvsa/testing/framework")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.dvsa.testing.framework")
 public class RunCucumberTests {
 }
