3736dfc72a9dd9b15bf6e9524c523334e5929b5f
Wonderson Chideya <wondersonchideya@gmail.com>
2023-01-05 13:29:10 +0000

    failsafe

--- src/test/java/org/dvsa/testing/framework/parallel/RunCucumberTests.java
+++ src/test/java/org/dvsa/testing/framework/parallel/RunCucumberTests.java
@@ -12,4 +12,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("org/dvsa/testing/framework")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.dvsa.testing.framework")
 public class RunCucumberTests {
+    public void something(){
+    }
 }
