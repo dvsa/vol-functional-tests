879cf5a4b269cbc87fe83645e34e729823f8e4a9
Stefan Ranoszek <stefanranoszek@icloud.com>
2023-02-22 11:11:03 +0000

    inital

--- src/test/java/org/dvsa/testing/framework/Journeys/licence/GovSignInJourney.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/licence/GovSignInJourney.java
@@ -34,9 +34,9 @@ public class GovSignInJourney extends BasePage {
     }
 
     public void signInGovAccount() {
+        String AUTH_KEY = world.configuration.config.getString("AUTH_KEY");
         String signInUsername = world.configuration.config.getString("signInUsername");
         String signInPassword = world.configuration.config.getString("signInPassword");
-        String AUTH_KEY = world.configuration.config.getString("AUTH_KEY");
 
         if(isTitlePresent("Prove your identity with a GOV.UK account", 1) &&
                 (isTextPresent("Choose a way to prove your identity"))) {
@@ -49,7 +49,7 @@ public class GovSignInJourney extends BasePage {
             waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
             waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
         }
-        if(isTitlePresent("You must have a photo ID to prove your identity with a GOV.UK account", 1)) {
+        if(isTitlePresent("You must have a photo ID to prove your identity with GOV.UK One Login", 1)) {
             photoIDQuestion();
         }
         if(isTitlePresent("Create a GOV.UK account or sign in",1)) {
@@ -70,7 +70,8 @@ public class GovSignInJourney extends BasePage {
     }
 
     public void registerGovAccount() {
-        if(isTitlePresent("Prove your identity with a GOV.UK account", 2)) {
+        String signInPassword = world.configuration.config.getString("signInPassword");
+        if(isTitlePresent("Prove your identity with GOV.UK One Login", 2)) {
             clickByXPath("//*[@id='form-tracking']/button");
         } else {
             clickById("chooseWayPyi");
@@ -80,9 +81,17 @@ public class GovSignInJourney extends BasePage {
         clickById("create-account-link");
         waitAndEnterText("email", SelectorType.ID, registrationEmail);
         waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
-/* @WIP
         waitAndEnterText("code", SelectorType.ID, world.configuration.getGovCode());
-*/
+        waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
+        waitAndEnterText("password", SelectorType.ID, signInPassword);
+        waitAndEnterText("confirm-password", SelectorType.ID, signInPassword);
+        waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
+        clickByXPath("//*[@id='mfaOptions-2']");
+        waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
+        waitAndClick("//*[@id='main-content']/div/div/details[2]/summary/span", SelectorType.XPATH);
+        getText("//*[@id='secret-key']", SelectorType.XPATH);
+        String secretCode = getTOTPCode(getText("//*[@id='secret-key']", SelectorType.XPATH));
+        waitAndEnterText("code", SelectorType.ID, secretCode);
         waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
         clickByXPath("//*[@id='havePhotoId']");
         waitAndClick("//*[contains(text(),'Continue')]", SelectorType.XPATH);
