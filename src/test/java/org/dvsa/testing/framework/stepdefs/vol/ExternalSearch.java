fe1b81df30266297732ded7375ddb20be718fc38
Wonderson Chideya <wondersonchideya@gmail.com>
2023-03-22 15:44:26 +0000

    fixed missing mapping

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/ExternalSearch.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/ExternalSearch.java
@@ -8,6 +8,7 @@ import org.dvsa.testing.framework.pageObjects.BasePage;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
 import org.dvsa.testing.lib.url.webapp.URL;
 import org.dvsa.testing.lib.url.webapp.utils.ApplicationType;
+import org.jetbrains.annotations.Nullable;
 import org.openqa.selenium.WebElement;
 
 import java.util.Objects;
@@ -17,20 +18,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ExternalSearch extends BasePage {
     private final World world;
 
-    public ExternalSearch(World world) {this.world = world;}
+    public ExternalSearch(World world) {
+        this.world = world;
+    }
 
     @And("i login as a partner user")
     public void iLoginAsAPartnerUser() {
         String user = world.configuration.config.getString("partnerUser");
         String password = world.configuration.config.getString("partnerUserPassword");
-        if(getDriver().getCurrentUrl().contains("dashboard")){
+        if (getDriver().getCurrentUrl().contains("dashboard")) {
             clickByLinkText("Sign out");
         }
         String externalURL = URL.build(ApplicationType.EXTERNAL, world.configuration.env, "auth/login").toString();
         get(externalURL);
         waitForTextToBePresent("Password");
-        world.globalMethods.signIn(user,password);
-        waitAndClick("Lorry and bus operators",SelectorType.PARTIALLINKTEXT);
+        world.globalMethods.signIn(user, password);
+        waitAndClick("Lorry and bus operators", SelectorType.PARTIALLINKTEXT);
     }
 
     @And("I am on the external search page")
@@ -38,39 +41,37 @@ public class ExternalSearch extends BasePage {
         world.selfServeNavigation.navigateToFindLorryAndBusOperatorsSearch();
     }
 
-        @Then("search results page addresses should only display address belonging to our post code")
+    @Then("search results page addresses should only display address belonging to our post code")
     public void searchResultsPageAddressesShouldOnlyDisplayAddressBelongingToOurPostCode() {
         String clippedCorrespondenceAddress = String.format("%s, %s, %s, %s",
-        world.createApplication.getCorrespondenceAddressLine3(),
-        world.createApplication.getCorrespondenceAddressLine4(),
-        world.createApplication.getCorrespondenceTown(),
-        world.createApplication.getCorrespondencePostCode()
+                world.createApplication.getCorrespondenceAddressLine3(),
+                world.createApplication.getCorrespondenceAddressLine4(),
+                world.createApplication.getCorrespondenceTown(),
+                world.createApplication.getCorrespondencePostCode()
         );
         String clippedOperatingCentreAddress = String.format("%s, %s, %s, %s",
-        world.createApplication.getOperatingCentreAddressLine3(),
-        world.createApplication.getOperatingCentreAddressLine4(),
-        world.createApplication.getOperatingCentreTown(),
-        world.createApplication.getOperatingCentrePostCode()
+                world.createApplication.getOperatingCentreAddressLine3(),
+                world.createApplication.getOperatingCentreAddressLine4(),
+                world.createApplication.getOperatingCentreTown(),
+                world.createApplication.getOperatingCentrePostCode()
         );
         world.selfServeNavigation.clickSearchWhileCheckingTextPresent(clippedCorrespondenceAddress, 240, "KickOut reached. Correspondence and operating centre address external search failed.");
-            WebElement tableRow = findElement(String.format("//tr[td[contains(text(),\"%s\")]]", clippedCorrespondenceAddress), SelectorType.XPATH);
-            assertTrue(tableRow.getText().contains(world.createApplication.getOrganisationName()));
-            assertTrue(tableRow.getText().contains(world.applicationDetails.getLicenceNumber()));
-            tableRow = findElement(String.format("//tr[td[contains(text(),\"%s\")]]", clippedOperatingCentreAddress), SelectorType.XPATH);
-            assertTrue(tableRow.getText().contains(world.createApplication.getOrganisationName()));
-            assertTrue(tableRow.getText().contains(world.applicationDetails.getLicenceNumber()));
-        }
+        WebElement tableRow = findElement(String.format("//tr[td[contains(text(),\"%s\")]]", clippedCorrespondenceAddress), SelectorType.XPATH);
+        assertTrue(tableRow.getText().contains(world.createApplication.getOrganisationName()));
+        assertTrue(tableRow.getText().contains(world.applicationDetails.getLicenceNumber()));
+        tableRow = findElement(String.format("//tr[td[contains(text(),\"%s\")]]", clippedOperatingCentreAddress), SelectorType.XPATH);
+        assertTrue(tableRow.getText().contains(world.createApplication.getOrganisationName()));
+        assertTrue(tableRow.getText().contains(world.applicationDetails.getLicenceNumber()));
+    }
 
-        @Then("search results page should display operator names containing our {string}")
-        public void searchResultsPageShouldDisplayOperatorNamesContainingOurBusinessName(String businessName) {
-            if (Objects.equals(world.configuration.env.toString(), "int") || (Objects.equals(world.configuration.env.toString(), "pp"))) {
-                world.selfServeNavigation.clickSearchWhileCheckingTextPresent(businessName, 300, "KickOut reached. Operator name external search failed.");
-                assertTrue(isTextPresent(businessName));
-            }
-            else if (Objects.equals(world.configuration.env.toString(), "qa") || (Objects.equals(world.configuration.env.toString(), "da")) || (Objects.equals(world.configuration.env.toString(), "reg"))) {
-                world.selfServeNavigation.clickSearchWhileCheckingTextPresent(world.createApplication.getOrganisationName(), 300, "KickOut reached. Operator name external search failed.");
-            }
+    @Then("search results page should display operator names containing our {string}")
+    public void searchResultsPageShouldDisplayOperatorNamesContainingOurBusinessName(String businessName) {
+        if (Objects.equals(world.configuration.env.toString(), "int") || (Objects.equals(world.configuration.env.toString(), "pp"))) {
+            world.selfServeNavigation.clickSearchWhileCheckingTextPresent(businessName, 300, "KickOut reached. Operator name external search failed.");
+            assertTrue(isTextPresent(businessName));
         }
+        world.selfServeNavigation.clickSearchWhileCheckingTextPresent(world.createApplication.getOrganisationName(), 300, "KickOut reached. Operator name external search failed.");
+    }
 
     @And("I am able to view the applicants licence number")
     public void iAmAbleToViewTheApplicantsLicenceNumber() {
@@ -94,7 +95,9 @@ public class ExternalSearch extends BasePage {
         assertTrue(tableRow.getText().contains(world.applicationDetails.getLicenceNumber()));
     }
 
+
     @When("I search for a lorry and bus operator by {string} with licence number {string}, business name {string}, person {string} and address {string}")
+    @When("I search for a lorry and bus operator by {string},{string},{string},{string},{string}")
     public void iSearchForALorryAndBusOperatorBy(String searchType, String licenceNumber, String businessName, String person, String address) {
         findSelectAllRadioButtonsByValue(searchType);
         String addressToSearch;
@@ -129,7 +132,7 @@ public class ExternalSearch extends BasePage {
         }
     }
 
-
+    @Then("search results page should only display our licence number")
     @Then("search results page should only display our {string}")
     public void searchResultsPageShouldOnlyDisplayOurLicenceNumber(String licenceNumber) {
         if (Objects.equals(world.configuration.env.toString(), "int") || (Objects.equals(world.configuration.env.toString(), "pp"))) {
--- src/test/resources/org/dvsa/testing/framework/features/SelfServe/Search/partner-user-external-search.feature
+++ src/test/resources/org/dvsa/testing/framework/features/SelfServe/Search/partner-user-external-search.feature
@@ -9,16 +9,16 @@ Feature: Partner user external search by Address, Business name, Licence Number 
 
   @smoketest
   Scenario: Business name partner external search for lorry and bus operators
-    When I search for a lorry and bus operator by "business"
-    Then search results page should display operator names containing our business name
+    When I search for a lorry and bus operator by "business","","","",""
+    Then search results page should display operator names containing our "business name"
     And I am able to view the applicants licence number
 
-    @search-lorry-by-licence
+  @search-lorry-by-licence
   Scenario: Licence number partner external search for lorry and bus operators
-    When I search for a lorry and bus operator by "licence"
-    Then search results page should only display our licence number
+    When I search for a lorry and bus operator by "licence","","","",""
+    Then search results page should only display our "licence number"
     And I am able to view the licence number
 
   Scenario: Person's name partner external search for lorry and bus operators
-    When I search for a lorry and bus operator by "person"
+    When I search for a lorry and bus operator by "person","","","",""
     Then search results page should display names containing our operator name
