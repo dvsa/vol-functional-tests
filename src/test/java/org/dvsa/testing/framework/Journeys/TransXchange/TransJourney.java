a7d3d11fc35188153a9abdd319936c7b1047272a
insacuri <manny160389@hotmail.com>
2023-05-24 12:28:40 +0100

    Commit progress after refactor

--- src/test/java/org/dvsa/testing/framework/Journeys/TransXchange/TransJourney.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/TransXchange/TransJourney.java
@@ -1,5 +1,11 @@
 package org.dvsa.testing.framework.Journeys.TransXchange;
 
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.URLConnectionClient;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
@@ -16,6 +22,9 @@ import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class TransJourney extends BasePage {
@@ -23,8 +32,10 @@ public class TransJourney extends BasePage {
     private World world;
     private URL url;
     private HttpURLConnection connection;
+    private String text;
 
     private final String VALID_XML_PATH = "./src/test/resources/org/dvsa/testing/framework/TransXchange/ValidPdfRequest.xml";
+    private final String VALID_XML_RESPONSE_PATH = "./src/test/resources/org/dvsa/testing/framework/TransXchange/ValidPdfResponse.xml";
     private final String INVALID_XML_PATH = "./src/test/resources/org/dvsa/testing/framework/TransXchange/InvalidPdfRequest.xml";
 
     public TransJourney(World world) {
@@ -48,44 +59,27 @@ public class TransJourney extends BasePage {
 
     public int sendValidXmlRequest() throws Exception {
 
+        final HttpPost request = new HttpPost(world.configuration.config.getString("apiUrl"));
         File xmlFile = new File(VALID_XML_PATH);
+        String xml = new String(Files.readAllBytes(xmlFile.toPath()));
+
         String token = world.TransXchangeJourney.getAuthToken();
-        url = new URL(world.configuration.config.getString("apiUrl"));
-        connection = (HttpURLConnection) url.openConnection();
+        request.setHeader("Authorization","Bearer " + token);
+        request.setHeader("X-Correlation-Id", "abc123");
+        request.setHeader("Content-Type", "application/xml");
+        request.setHeader("Cache-Control","no-cache");
+        request.setEntity(new StringEntity(xml));
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        ClassicHttpResponse response = client.execute(request, responseHandler -> {
+            text = EntityUtils.toString(responseHandler.getEntity());
+          return responseHandler;
+        });
 
-        connection.setRequestProperty("Authorization","Bearer " + token);
-        connection.setRequestMethod("POST");
-        configureConnection();
-
-        connection.setDoOutput(true);
-
-        FileInputStream fileInputStream = new FileInputStream(xmlFile);
-
-        // Get the output stream of the connection
-        OutputStream outputStream = connection.getOutputStream();
-
-        // Write the XML file contents to the output stream
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
-        }
-
-        outputStream.flush();
-        outputStream.close();
-
-        fileInputStream.close();
-
-        return connection.getResponseCode();
-
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            // Request successful
-//            System.out.println("XML file sent successfully.");
-//        } else {
-//            // Request failed
-//            System.out.println("Failed to send XML file. Response Code: " + responseCode);
-//        }
-
+        // Test the response is what we expect
+        File xmlResponseFile = new File(VALID_XML_RESPONSE_PATH);
+        String xmlResponse = new String(Files.readAllBytes(xmlResponseFile.toPath()));
+        assertEquals(text, xmlResponse);
+        return response.getCode();
     }
 
     public int sendInvalidXmlRequest() throws Exception {
