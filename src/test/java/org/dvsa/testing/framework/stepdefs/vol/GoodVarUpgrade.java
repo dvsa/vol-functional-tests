84333497e9f11947306b070e07e208ee93adc80a
Neil Culshaw <neil.culshaw@dvsa.gov.uk>
2023-09-14 11:43:30 +0100

    Assertion improved and tags added

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/GoodVarUpgrade.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/GoodVarUpgrade.java
@@ -1,9 +1,12 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.dvsa.testing.framework.Injectors.World;
 import org.dvsa.testing.framework.pageObjects.BasePage;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GoodVarUpgrade extends BasePage {
     private final World world;
@@ -15,5 +18,23 @@ public class GoodVarUpgrade extends BasePage {
         world.generalVariationJourney.beginUpgradeVariation();
         click(world.typeOfLicenceJourney.standardNational, SelectorType.XPATH);
         world.UIJourney.clickSaveAndReturn();
+    }
+
+    @Then("correct statuses are shown by the correct seven sections")
+    public void correctStatusesAreShownByTheCorrectSevenSections() {
+        String typeOfLicenceStatus = getText("//a[@id='overview-item__type_of_licence']//strong[1]", SelectorType.XPATH);
+        assertTrue(typeOfLicenceStatus.contains("UPDATED"));
+        String addressesStatus = getText("//a[@id='overview-item__addresses']//strong[1]", SelectorType.XPATH);
+        assertTrue(addressesStatus.contains("REQUIRES ATTENTION"));
+        String financialEvidenceStatus = getText("//a[@id='overview-item__financial_evidence']//strong[1]", SelectorType.XPATH);
+        assertTrue(financialEvidenceStatus.contains("REQUIRES ATTENTION"));
+        String transportManagersStatus = getText("//a[@id='overview-item__transport_managers']//strong[1]", SelectorType.XPATH);
+        assertTrue(transportManagersStatus.contains("REQUIRES ATTENTION"));
+        String financialHistoryStatus = getText("//a[@id='overview-item__financial_history']//strong[1]",SelectorType.XPATH);
+        assertTrue(financialHistoryStatus.contains("REQUIRES ATTENTION"));
+        String convictionsPenaltiesStatus = getText("//a[@id='overview-item__convictions_penalties']//strong[1]", SelectorType.XPATH);
+        assertTrue(convictionsPenaltiesStatus.contains("REQUIRES ATTENTION"));
+        String reviewDeclarationsStatus = getText("//div[@class='overview__item']//strong[1]", SelectorType.XPATH);
+        assertTrue(reviewDeclarationsStatus.contains("CAN'T START YET"));
     }
 }
--- src/test/resources/org/dvsa/testing/framework/features/SelfServe/Variations/goods-variation-upgrade.feature
+++ src/test/resources/org/dvsa/testing/framework/features/SelfServe/Variations/goods-variation-upgrade.feature
@@ -1,6 +1,9 @@
+@ss_regression
+@SS-GOODS-VAR-UPGRADE
+
 Feature: Goods variation upgrade of licence type
 
   Scenario: Goods variation to upgrade from Restricted to Standard National
     Given i have a valid "goods" "restricted" licence
     When i upgrade my licence type to Standard National
-    Then a status of update required should be shown next to financial evidence
+    Then correct statuses are shown by the correct seven sections
