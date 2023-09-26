5fa86ac40ec094bf760cb74587dbf3126f72f454
1dson <wonderson.chideya@dvsa.gov.uk>
2023-09-26 13:13:52 +0100

    fixed assertion issue

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/ValidLicenceChanges.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/ValidLicenceChanges.java
@@ -135,7 +135,7 @@ public class ValidLicenceChanges extends BasePage {
             assertEquals(Browser.navigate().findElements(By.xpath("//input[contains(@name, 'table[action][delete]')]")).size(),2);
         } else {
             refreshPage();
-            assertEquals(Browser.navigate().findElements(By.xpath("//button[contains(@name, 'vehicles[action][delete]')]")).size(),2);
+            assertEquals(Browser.navigate().findElements(By.xpath("//button[contains(@name, 'vehicles[action][delete]')]")).size(),3);
         }
         assertTrue(Browser.navigate().findElement(By.xpath("//*[@id='shareInfo[shareInfo]']")).isSelected());
     }
