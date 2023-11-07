119cca43a29b76c8c853793b27579bb7e20baad3
Neil Culshaw <neil.culshaw@dvsa.gov.uk>
2023-11-07 17:29:10 +0000

    WIP initial tab display assertions

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/Messaging.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/Messaging.java
@@ -1,14 +1,19 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
 import io.cucumber.java.en.Then;
+import org.dvsa.testing.framework.pageObjects.BasePage;
 
-public class Messaging {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Messaging extends BasePage {
     @Then("the messaging tab should be displayed")
     public void theMessagingTabShouldBeDisplayed() {
-        
+        assertTrue(isTextPresent("Messages"));
     }
 
     @Then("the messaging tab should not be displayed")
     public void theMessagingTabShouldNotBeDisplayed() {
+        assertFalse(isTextPresent("Messages"));
     }
 }
