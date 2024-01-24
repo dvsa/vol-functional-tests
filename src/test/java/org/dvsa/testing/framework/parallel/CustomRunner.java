4b84787c473c6ed18a6699ed90e2c0d466c9547f
1dson <wonderson.chideya@dvsa.gov.uk>
2024-01-24 16:26:19 +0000

    2 runners/2 forks

--- src/test/java/org/dvsa/testing/framework/parallel/CustomRunner.java
+++ src/test/java/org/dvsa/testing/framework/parallel/CustomRunner.java
@@ -45,6 +45,6 @@ public class CustomRunner implements ParallelExecutionConfiguration, ParallelExe
 
     @Override
     public int getKeepAliveSeconds() {
-        return 50;
+        return 5;
     }
 }
