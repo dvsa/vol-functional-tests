bac9534e7a3dcd863d2feb8196471b1924b92d09
Stefan Ranoszek <stefan48500@gmail.com>
2024-04-24 14:36:18 +0100

    feat: using aws secrets manager

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/CreateNewOperatorLicense.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/CreateNewOperatorLicense.java
@@ -1,5 +1,6 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
+import activesupport.aws.s3.SecretsManager;
 import io.cucumber.java.en.When;
 import org.dvsa.testing.framework.Injectors.World;
 import io.cucumber.java.en.And;
@@ -43,8 +44,8 @@ public class CreateNewOperatorLicense extends BasePage {
 
     @When("I log in as a Local Authority User")
     public void iLogInAsALocalAuthorityUser() {
-        String user = world.configuration.config.getString("localAuthorityUser");
-        String password = world.configuration.config.getString("defaultPassword");
+        String user = SecretsManager.getSecretValue("localAuthorityUser");
+        String password = SecretsManager.getSecretValue("defaultPassword");
 
         if (getDriver().getCurrentUrl().contains("dashboard")) {
             clickByLinkText("Sign out");
