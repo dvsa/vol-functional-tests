8a7d00ae673db6933e45b4ea626f7e40f0b76745
sneha <sneha.kambale@dvsa.gov.uk>
2024-10-24 14:44:01 +0100

    fix: message scenarios fix as part of assets

--- src/test/java/org/dvsa/testing/framework/Journeys/licence/MessagingJourney.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/licence/MessagingJourney.java
@@ -48,18 +48,19 @@ public class MessagingJourney extends BasePage {
     }
 
     public void archiveTheConversation() {
-        click("//button[@id='close']", SelectorType.XPATH);
+        waitAndClick("//button[@id='close']", SelectorType.XPATH);
         waitForTextToBePresent("End Conversation");
         click("//button[@id='close']", SelectorType.XPATH);
-        waitForTextToBePresent("CLOSED");
-        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--grey')]", SelectorType.XPATH), "CLOSED");
+        waitForTextToBePresent("Closed");
+        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--grey')]", SelectorType.XPATH), "Closed");
     }
 
     public void createNewConversation() {
         clickByLinkText("Start a new conversation");
         selectRandomValueFromDropDown("//*[@id='form-actions[inputs][messageSubject]']", SelectorType.XPATH);
         click("//*[@id='form-actions[inputs][appOrLicNo]']", SelectorType.XPATH);
-        click("//*[@id='form-actions[inputs][appOrLicNo]']/optgroup[1]/option ", SelectorType.XPATH);
+
+        click("//*[@id=\"form-actions[inputs][appOrLicNo]\"]/optgroup[2]/option", SelectorType.XPATH);
         waitAndEnterText("//*[@id='form-actions[inputs][messageContent]']", SelectorType.XPATH, Str.randomWord(10));
         clickById("send");
     }
@@ -70,19 +71,19 @@ public class MessagingJourney extends BasePage {
     }
 
     public void viewNewMessage() {
-        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--red')]", SelectorType.XPATH), "NEW MESSAGE");
+        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--red')]", SelectorType.XPATH), "New message");
         click("//*[@id='main-content']//tbody/tr[1]/td[1]/a", SelectorType.XPATH);
     }
 
     public void openMessageStatusCheck() {
         clickByLinkText("Messages");
-        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--blue')]", SelectorType.XPATH), "OPEN");
+        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--blue')]", SelectorType.XPATH), "Open");
     }
 
     public void notificationCount() {
         assertEquals(getText("//*[contains(@class,'notification-count__number')]", SelectorType.XPATH), "1");
         clickByLinkText("Messages");
-        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--red')]", SelectorType.XPATH), "NEW MESSAGE");
+        assertEquals(getText("//*[contains(@class,'govuk-tag govuk-tag--red')]", SelectorType.XPATH), "New message");
     }
 
     public void backToConversation() {
