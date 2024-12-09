53244c61884b0e139a41ad3efa4629c5dfde8ec1
Stefan Ranoszek <stefan48500@gmail.com>
2024-12-09 11:10:01 +0000

    chore: sout

--- src/test/java/org/dvsa/testing/framework/Global/GlobalMethods.java
+++ src/test/java/org/dvsa/testing/framework/Global/GlobalMethods.java
@@ -1,6 +1,9 @@
 package org.dvsa.testing.framework.Global;
 
 import activesupport.aws.s3.SecretsManager;
+import org.apache.commons.logging.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dvsa.testing.framework.Injectors.World;
 import activesupport.dates.Dates;
 import activesupport.dates.LocalDateCalendar;
@@ -20,6 +23,7 @@ import static activesupport.driver.Browser.navigate;
 
 public class GlobalMethods extends BasePage {
 
+    private static final Logger LOGGER = LogManager.getLogger(GlobalMethods.class);
     private World world;
     private String loginPassword;
     public Dates date = new Dates(new LocalDateCalendar());
@@ -67,8 +71,9 @@ public class GlobalMethods extends BasePage {
         String password;
         QuotedPrintableCodec quotedPrintableCodec = new QuotedPrintableCodec();
         //if (!world.configuration.env.toString().equals("local")) {
-            password = quotedPrintableCodec.decode(world.configuration.getTempPassword(emailAddress));
-        System.out.println(password + "here is the pw");
+       //     password = quotedPrintableCodec.decode(world.configuration.getTempPassword(emailAddress));
+        LOGGER.info("PASSWORD");
+
 /*        } else {
             throw new IllegalStateException("getTempPasswordFromMailhog method is missing");
         }
@@ -76,20 +81,20 @@ public class GlobalMethods extends BasePage {
             throw new IllegalArgumentException("Retrieved password is null");
         }*/
 
-        try {
-            signIn(username, password);
-            if (isTextPresent("Please check your username and password")) {
-                signIn(username, getLoginPassword());
-            }
-        } finally {
-            if (isTextPresent("Your password must:")) {
-                waitAndEnterText(newPasswordField, SelectorType.CSS, newPassword);
-                waitAndEnterText(confirmPasswordField, SelectorType.CSS, newPassword);
-                click(nameAttribute("input", "submit"), SelectorType.CSS);
-                setLoginPassword(newPassword);
-                untilNotInDOM(submitButton, 1);
-            }
-        }
+//        try {
+//            signIn(username, password);
+//            if (isTextPresent("Please check your username and password")) {
+//                signIn(username, getLoginPassword());
+//            }
+//        } finally {
+//            if (isTextPresent("Your password must:")) {
+//                waitAndEnterText(newPasswordField, SelectorType.CSS, newPassword);
+//                waitAndEnterText(confirmPasswordField, SelectorType.CSS, newPassword);
+//                click(nameAttribute("input", "submit"), SelectorType.CSS);
+//                setLoginPassword(newPassword);
+//                untilNotInDOM(submitButton, 1);
+//            }
+//        }
     }
     public void signIn(String userName, String password) {
         if (isElementPresent("declarationRead", SelectorType.ID)
