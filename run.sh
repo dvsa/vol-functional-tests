805f92d967b5c957d915204180b9f4257e0866f8
wonderson chideya <wonderson.chideya2@dvsa.gov.uk>
2025-01-14 17:03:56 +0000

    add step to cache dependencies

--- run.sh
+++ run.sh
@@ -72,13 +72,12 @@ if [ $? -eq 0 ]; then
   mvn allure:report
   # create the report zip file
   zip -qr allure.zip target
+  mv allure.zip ./allure_attempt_${github.run_number}.zip
   cd target
   aws s3 cp site s3://${resultsTargetBucket}/${resultsTargetBucketPath}/${buildId}/site/ --recursive
-  aws s3 cp ../allure.zip s3://${resultsTargetBucket}/${resultsTargetBucketPath}/${buildId}/
+  aws s3 cp ../allure_attempt_${github.run_number}.zip s3://${resultsTargetBucket}/${resultsTargetBucketPath}/${buildId}/
 else
   echo "Maven command failed!"
 fi
-
-
 
 
