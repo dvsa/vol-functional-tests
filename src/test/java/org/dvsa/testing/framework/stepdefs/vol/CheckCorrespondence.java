1229a0360bbcdbb99a4073ae607339349fef3d89
wonderson chideya <wonderson.chideya2@dvsa.gov.uk>
2025-01-27 12:21:46 +0000

    fix:changed the way the user logs in

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/CheckCorrespondence.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/CheckCorrespondence.java
@@ -1,18 +1,13 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
 import activesupport.aws.s3.SecretsManager;
-import org.apache.hc.core5.http.HttpException;
-import org.bouncycastle.oer.Switch;
 import org.dvsa.testing.framework.Injectors.World;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import org.dvsa.testing.framework.enums.Realm;
 import org.dvsa.testing.framework.pageObjects.BasePage;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
-
-import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -24,31 +19,15 @@ public class CheckCorrespondence extends BasePage {
     }
 
     @And("i have logged in to self serve as {string}")
-    public void iHaveLoggedInToSelfServe(String userType) throws HttpException {
+    public void iHaveLoggedInToSelfServe(String userType) {
+        world.selfServeNavigation.navigateToLoginPage();
         if (userType.equalsIgnoreCase("consultant")) {
-            if (world.token.getToken(world.registerConsultantAndOperator.getConsultantDetails().getUserName(),
-                    SecretsManager.getSecretValue("internalNewPassword"), Realm.EXTERNAL.getServiceType()) == null) {
-                world.selfServeNavigation.navigateToLogin(
-                        world.registerConsultantAndOperator.getConsultantDetails().getUserName(),
-                        world.registerConsultantAndOperator.getConsultantDetails().getEmailAddress()
-                );
-            } else {
-                world.selfServeNavigation.navigateToLoginPage();
-                world.globalMethods.signIn(world.registerConsultantAndOperator.getConsultantDetails().getUserName(),
-                        SecretsManager.getSecretValue("internalNewPassword"));
-            }
+            world.globalMethods.signIn(world.registerConsultantAndOperator.getConsultantDetails().getUserName(),
+                    SecretsManager.getSecretValue("internalNewPassword"));
         } else {
-            if (world.token.getToken(world.registerUser.getUserName(),
-                    SecretsManager.getSecretValue("internalNewPassword"), Realm.EXTERNAL.getServiceType()) == null) {
-                world.selfServeNavigation.navigateToLogin(
-                        world.registerUser.getUserName(),
-                        world.registerUser.getEmailAddress()
-                );
-            } else {
-                world.selfServeNavigation.navigateToLoginPage();
-                world.globalMethods.signIn(world.registerUser.getUserName(),
-                        SecretsManager.getSecretValue("internalNewPassword"));
-            }
+            world.globalMethods.signIn(world.registerUser.getUserName(),
+                    SecretsManager.getSecretValue("internalNewPassword"));
+
         }
     }
 
