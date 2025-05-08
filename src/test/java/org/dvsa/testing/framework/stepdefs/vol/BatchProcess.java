41a55a39793c1e0d60b5718d90b5844753da3a23
Stefan Ranoszek <stefan48500@gmail.com>
2025-05-08 11:22:39 +0100

    chore: select dropdown

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/BatchProcess.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/BatchProcess.java
@@ -46,7 +46,6 @@ public class BatchProcess extends BasePage {
     @When("i add a new bus registration with a past date")
     public void iAddANewBusRegistrationWithAPastDate() {
         world.internalUIJourney.manualBusRegistration(0,0,1);
-        // world.internalUIJourney.payFee();
     }
 
     @And("i trigger the expire-bus-registration batch job")
