cb0519bb15fd957c14f72183bdb8c967f53e4d01
Stefan Ranoszek <stefan48500@gmail.com>
2025-05-23 13:58:11 +0100

    fix:confirm

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/InterimLicence.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/InterimLicence.java
@@ -138,6 +138,7 @@ public class InterimLicence extends BasePage {
         click("//*[@name='form-actions[submitAndPay]']", SelectorType.XPATH);
         UniversalActions.clickPay();
         world.feeAndPaymentJourney.customerPaymentModule();
+        waitAndClick("confirm", SelectorType.ID);
         waitForTitleToBePresent("Application overview");
     }
 
