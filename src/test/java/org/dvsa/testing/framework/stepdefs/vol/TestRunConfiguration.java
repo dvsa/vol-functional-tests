9729decb0fe367c07b3ac804298ef7dd1d241e40
Stefan Ranoszek <stefan48500@gmail.com>
2025-07-17 11:17:43 +0100

    Revert "fix: revert"
    This reverts commit 4820d7fb79a46fa088c9e0b0adfc04fafaf36e3b.

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/TestRunConfiguration.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/TestRunConfiguration.java
@@ -10,25 +10,72 @@ import org.dvsa.testing.framework.Report.Config.Environments;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
 import java.io.ByteArrayInputStream;
 import java.util.Collection;
-
 import static org.dvsa.testing.framework.pageObjects.BasePage.isLinkPresent;
 import static org.dvsa.testing.framework.pageObjects.BasePage.waitAndClick;
 
 public class TestRunConfiguration {
+    private static final ThreadLocal<Integer> retryCount = new ThreadLocal<>();
+    private static final ThreadLocal<String> currentScenarioName = new ThreadLocal<>();
+    private static final int MAX_RETRIES = 1;
+
     @Before
     public void setUp(Scenario scenario) throws Exception {
+        if (retryCount.get() == null) {
+            retryCount.set(0);
+        }
+        currentScenarioName.set(scenario.getName());
+
         Environments environments = new Environments();
         environments.createResultsFolder();
         environments.generateXML();
-        System.out.println("Testing Scenario:" + scenario.getName());
+
+        int currentRetry = retryCount.get();
+        if (currentRetry > 0) {
+            System.out.println("RETRY " + currentRetry + "/" + MAX_RETRIES + " - Testing Scenario: " + scenario.getName());
+        } else {
+            System.out.println("Testing Scenario: " + scenario.getName());
+        }
     }
 
     @After
     public void takeScreenShotAfterFailure(Scenario scenario) {
-        attach(scenario);
+        boolean scenarioFailed = scenario.isFailed();
+        if (scenarioFailed) {
+            attach(scenario);
+
+            int currentRetry = retryCount.get();
+            if (currentRetry < MAX_RETRIES) {
+                System.out.println("Scenario failed, will retry... (attempt " + (currentRetry + 1) + "/" + MAX_RETRIES + ")");
+                retryCount.set(currentRetry + 1);
+
+                cleanupForRetry();
+            } else {
+                System.out.println("Scenario failed after " + MAX_RETRIES + " retries.");
+            }
+        }
+
+        performFinalCleanup(scenario);
+
+
+        retryCount.remove();
+        currentScenarioName.remove();
+    }
+
+    private void cleanupForRetry() {
+        try {
+            if (Browser.isBrowserOpen()) {
+                Browser.getDriver().manage().deleteAllCookies();
+                Browser.getDriver().navigate().refresh();
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error during retry cleanup: " + e.getMessage());
+        }
+    }
+
+    private void performFinalCleanup(Scenario scenario) {
         Collection<String> tags = scenario.getSourceTagNames();
         for (String tag : tags) {
             if (Browser.isBrowserOpen() && tag.contains("readOnly")) {
@@ -37,6 +84,7 @@ public class TestRunConfiguration {
                 }
             }
         }
+
         if (Browser.isBrowserOpen()) {
             Browser.getDriver().quit();
             Browser.removeLocalDriverThread();
@@ -44,11 +92,16 @@ public class TestRunConfiguration {
     }
 
     public static void attach(Scenario scenarioStatus) {
+
         if (scenarioStatus.isFailed()) {
-            Allure.addAttachment("Oops something has gone wrong",
-                    new ByteArrayInputStream(((TakesScreenshot) Browser.navigate()).getScreenshotAs(OutputType.BYTES)));
-                    var currentUrl = Browser.getDriver().getCurrentUrl();
-             Allure.addAttachment("Current URL", currentUrl);
-        }
+            try {
+                Allure.addAttachment("Oops something has gone wrong",
+                        new ByteArrayInputStream(((TakesScreenshot) Browser.navigate()).getScreenshotAs(OutputType.BYTES)));
+                var currentUrl = Browser.getDriver().getCurrentUrl();
+                Allure.addAttachment("Current URL", currentUrl);
+            } catch (Exception e) {
+                System.out.println("Could not attach screenshot: " + e.getMessage());
+            }
         }
     }
+}
