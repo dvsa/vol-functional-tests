ad5fcf9066cf8366a1b1d72cbe3be2f2cc499ca6
Neil Culshaw <neil.culshaw@dvsa.gov.uk>
2025-08-06 15:55:07 +0100

    chore: removed unnecessary import

--- src/test/java/org/dvsa/testing/framework/stepdefs/vol/SelfServeNavigation.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/vol/SelfServeNavigation.java
@@ -1,7 +1,6 @@
 package org.dvsa.testing.framework.stepdefs.vol;
 
 import activesupport.aws.s3.SecretsManager;
-import io.cucumber.java.en.Then;
 import org.dvsa.testing.framework.Injectors.World;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
