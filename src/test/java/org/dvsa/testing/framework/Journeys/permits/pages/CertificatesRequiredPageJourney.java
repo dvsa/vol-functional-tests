dea0ab5659bf0f515a5103c693dd508cd8650ac6
Stefan Ranoszek <stefan48500@gmail.com>
2025-11-05 08:00:09 +0000

    fix: mandate

--- src/test/java/org/dvsa/testing/framework/Journeys/permits/pages/CertificatesRequiredPageJourney.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/permits/pages/CertificatesRequiredPageJourney.java
@@ -2,6 +2,7 @@ package org.dvsa.testing.framework.Journeys.permits.pages;
 
 import org.dvsa.testing.framework.Injectors.World;
 import org.dvsa.testing.framework.Journeys.permits.BasePermitJourney;
+import org.dvsa.testing.framework.Utils.Generic.UniversalActions;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
 import org.dvsa.testing.framework.pageObjects.external.pages.CertificatesRequiredPage;
 
@@ -12,8 +13,9 @@ public class CertificatesRequiredPageJourney extends BasePermitJourney {
     }
     public static void completePage() {
         waitForPageLoad();
+        waitForElementToBeClickable("//input[@id='qaElement']", SelectorType.XPATH);
         clickByXPath("//input[@id='qaElement']");
-        saveAndContinue();
+        UniversalActions.clickSaveAndContinue();
     }
 
 }
