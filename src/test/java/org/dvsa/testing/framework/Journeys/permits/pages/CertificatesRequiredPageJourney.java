48f920b0772cbfe362fe863c3460ddfc3b43fcb0
Stefan Ranoszek <stefan48500@gmail.com>
2025-11-05 12:02:46 +0000

    chore: wait

--- src/test/java/org/dvsa/testing/framework/Journeys/permits/pages/CertificatesRequiredPageJourney.java
+++ src/test/java/org/dvsa/testing/framework/Journeys/permits/pages/CertificatesRequiredPageJourney.java
@@ -3,8 +3,10 @@ package org.dvsa.testing.framework.Journeys.permits.pages;
 import org.dvsa.testing.framework.Injectors.World;
 import org.dvsa.testing.framework.Journeys.permits.BasePermitJourney;
 import org.dvsa.testing.framework.Utils.Generic.UniversalActions;
+import org.dvsa.testing.framework.pageObjects.BasePage;
 import org.dvsa.testing.framework.pageObjects.enums.SelectorType;
 import org.dvsa.testing.framework.pageObjects.external.pages.CertificatesRequiredPage;
+import org.dvsa.testing.framework.pageObjects.external.pages.baseClasses.BasePermitPage;
 
 public class CertificatesRequiredPageJourney extends BasePermitJourney {
 
@@ -13,10 +15,10 @@ public class CertificatesRequiredPageJourney extends BasePermitJourney {
     }
     public static void completePage() {
         CertificatesRequiredPage.untilOnPage();
-        refreshPage();
-        waitForElementToBeClickable("//input[@id='qaElement']", SelectorType.XPATH);
-        clickByXPath("//input[@id='qaElement']");
-        UniversalActions.clickSaveAndContinue();
+        CertificatesRequiredPage.confirmCertificateRequired();
+        BasePage.waitForElementToBePresent("//fieldset[@data-group='qa[fieldset36]']//input[@id='qaElement' and @type='checkbox']");
+        saveAndContinue();
+        BasePage.waitForElementToBePresent("//input[contains(@id,'_day')]");
+        BasePage.waitForElementNotToBePresent("//h1[contains(text(), 'Mandatory certificates')]");
     }
-
 }
