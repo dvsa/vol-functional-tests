feccc843ab767626488d267ae017a8da99dc8201
Stefan Ranoszek <stefan48500@gmail.com>
2025-12-05 09:46:21 +0000

    feat: retry

--- src/test/java/org/dvsa/testing/framework/parallel/ReRunFailedCucumberTests.java
+++ src/test/java/org/dvsa/testing/framework/parallel/ReRunFailedCucumberTests.java
@@ -6,13 +6,17 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test runner for executing only previously failed scenarios.
+ * Reads scenario locations from the rerun file generated during initial execution.
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("org/dvsa/testing/framework")
-@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/org/dvsa/testing/framework/features")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "target/rerun.txt")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "org.dvsa.testing.framework")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty, junit:target/results-summary/summary.xml, rerun:target/rerun.txt")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty, junit:target/results-summary/summary-rerun.xml")
 
-public class RunCucumberTests {
+public class ReRunFailedCucumberTests {
 }
