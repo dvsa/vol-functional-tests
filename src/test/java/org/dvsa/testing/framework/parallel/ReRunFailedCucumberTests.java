d9dcf3249653f1654f2abc53cef8a0e0389083ca
Stefan Ranoszek <stefan48500@gmail.com>
2025-12-09 11:52:41 +0000

    chore: allure

--- src/test/java/org/dvsa/testing/framework/parallel/ReRunFailedCucumberTests.java
+++ src/test/java/org/dvsa/testing/framework/parallel/ReRunFailedCucumberTests.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "@target/rerun.txt",
         glue = {"org.dvsa.testing.framework.stepdefs"},
         plugin = {
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm:target/allure-results",
                 "pretty",
                 "html:target/cucumber-reports/retry-html",
                 "json:target/cucumber-reports/retry-json/Cucumber.json"
